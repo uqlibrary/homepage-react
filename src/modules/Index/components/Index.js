0abbae33d2aca31461d53ab1b6d67dcca984bfb4
Ky <k.lane@uq.edu.au>
2020-08-19 13:56:49 +1000

    Fix layout

--- src/modules/Index/components/Index.js
+++ src/modules/Index/components/Index.js
@@ -1,19 +1,17 @@
 import React, { PureComponent } from 'react';
 
 import { StandardPage } from 'modules/SharedComponents/Toolbox/StandardPage';
+import { StandardCard } from 'modules/SharedComponents/Toolbox/StandardCard';
 
 import { withStyles } from '@material-ui/core/styles';
-import Grid from '@material-ui/core/Grid';
 
 class IndexComponent extends PureComponent {
     render() {
         return (
-            <StandardPage title={'Hello'}>
-                <Grid container>
-                    <Grid item xs={12} md={8} style={{ margin: '0 24px 300px 64px' }}>
-                        <p>Welcome to the new homepage.</p>
-                    </Grid>
-                </Grid>
+            <StandardPage title="Welcome">
+                <div className="layout-card">
+                    <StandardCard title={'To the new UQ Library Homepage'}>Welcome to the new homepage.</StandardCard>
+                </div>
             </StandardPage>
         );
     }
