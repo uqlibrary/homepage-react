0ff8527ae5bd4968e543ac23c3b85b42db2e28ac
Ky Lane <k.lane@uq.edu.au>
2020-09-21 16:10:33 +1000

    [#174395149] Changes to chat status

--- src/modules/App/components/ChatStatus.js
+++ src/modules/App/components/ChatStatus.js
@@ -2,16 +2,34 @@ import React, { useState, useEffect } from 'react';
 import { makeStyles } from '@material-ui/styles';
 import Button from '@material-ui/core/Button';
 import Snackbar from '@material-ui/core/Snackbar';
-import SnackbarContent from '@material-ui/core/SnackbarContent';
 import IconButton from '@material-ui/core/IconButton';
+import Grid from '@material-ui/core/Grid';
 import CloseIcon from '@material-ui/icons/Close';
 import PropTypes from 'prop-types';
+import QuestionAnswerIcon from '@material-ui/icons/QuestionAnswer';
+import Fab from '@material-ui/core/Fab';
+import Tooltip from '@material-ui/core/Tooltip';
 
 const useStyles = makeStyles(
     theme => ({
         chatStatus: {
-            backgroundColor: theme.palette.secondary,
+            position: 'absolute',
+            bottom: theme.spacing(2),
+            right: theme.spacing(2),
+            backgroundColor: 'rgba(0,0,0,0.75)',
+            color: theme.palette.white.main,
             textTransform: 'uppercase',
+            paddingTop: 8,
+            paddingBottom: 8,
+            paddingLeft: 16,
+            paddingRight: 16,
+            // width: 340,
+            borderRadius: 4,
+        },
+        chatAction: {
+            position: 'absolute',
+            bottom: theme.spacing(2),
+            right: theme.spacing(2),
         },
     }),
     { withTheme: true },
@@ -27,6 +45,9 @@ export const ChatStatus = ({ status }) => {
     const closeChatStatus = () => {
         setCloseChatState(true);
     };
+    const openChatStatus = () => {
+        setCloseChatState(false);
+    };
     const launchChat = () => {
         window.open(
             'https://support.my.uq.edu.au/app/chat/chat_launch_lib/p/45',
@@ -35,31 +56,50 @@ export const ChatStatus = ({ status }) => {
         );
         return false;
     };
-    return (
-        <Snackbar
-            anchorOrigin={{
-                vertical: 'bottom',
-                horizontal: 'right',
-            }}
-            open={chatState.online && !closeChatState}
-            onClose={closeChatStatus}
-        >
-            <SnackbarContent
+    if (chatState.online && !closeChatState) {
+        return (
+            <Snackbar
+                anchorOrigin={{
+                    vertical: 'bottom',
+                    horizontal: 'right',
+                }}
                 className={classes.chatStatus}
-                message="Online chat available"
-                action={
-                    <React.Fragment>
+                open
+                onClose={closeChatStatus}
+            >
+                <Grid container spacing={2} alignContent={'center'} alignItems={'center'}>
+                    <Grid item xs>
+                        Online&nbsp;chat&nbsp;available
+                    </Grid>
+                    <Grid item xs={'auto'}>
                         <Button color="primary" size="small" variant="contained" onClick={launchChat}>
                             Launch
                         </Button>
+                    </Grid>
+                    <Grid item xs={'auto'} style={{ marginLeft: -8, marginRight: -8 }}>
                         <IconButton size="small" aria-label="close" color="inherit" onClick={closeChatStatus}>
                             <CloseIcon fontSize="small" />
                         </IconButton>
-                    </React.Fragment>
-                }
-            />
-        </Snackbar>
-    );
+                    </Grid>
+                </Grid>
+            </Snackbar>
+        );
+    }
+    if (chatState.online && closeChatState) {
+        return (
+            <Tooltip
+                id="auth-button"
+                title={'Click to open online chat'}
+                placement="left"
+                TransitionProps={{ timeout: 300 }}
+            >
+                <Fab color="secondary" className={classes.chatAction} onClick={openChatStatus} size={'small'}>
+                    <QuestionAnswerIcon />
+                </Fab>
+            </Tooltip>
+        );
+    }
+    return null;
 };
 
 ChatStatus.propTypes = {
