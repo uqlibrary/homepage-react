bced68c51e65e991311a93ab06bfda0e2c99ce1f
Lea de Groot <l.degroot@uq.edu.au>
2020-10-22 10:05:47 +1000

    extract TabPanel to component [PT #174524208]

--- src/pages/CourseResources/components/CourseResources.js
+++ src/pages/CourseResources/components/CourseResources.js
@@ -8,45 +8,18 @@ import { Guides } from './Guides';
 import { LearningResources } from './LearningResources';
 import { PastExamPapers } from './PastExamPapers';
 import { SpacedArrowForwardIcon } from './SpacedArrowForwardIcon';
+import { TabPanel } from './TabPanel';
 
 import { StandardPage } from 'modules/SharedComponents/Toolbox/StandardPage';
 import { StandardCard } from 'modules/SharedComponents/Toolbox/StandardCard';
 import PrimoSearch from 'modules/reusable/PrimoSearch/containers/PrimoSearch';
 
 import AppBar from '@material-ui/core/AppBar';
-import Box from '@material-ui/core/Box';
 import Grid from '@material-ui/core/Grid';
 import InputLabel from '@material-ui/core/InputLabel';
 import Tab from '@material-ui/core/Tab';
 import Tabs from '@material-ui/core/Tabs';
 import Typography from '@material-ui/core/Typography';
-
-function TabPanel(props) {
-    const { children, value, tabId, index, ...other } = props;
-
-    return (
-        <Grid
-            role="tabpanel"
-            hidden={value !== index}
-            id={tabId || `tabpanel-${index}`}
-            aria-labelledby={tabId || `tabpanel-${index}`}
-            {...other}
-        >
-            {value === index && (
-                <Box p={3} style={{ paddingLeft: 0, paddingRight: 0 }}>
-                    {children}
-                </Box>
-            )}
-        </Grid>
-    );
-}
-
-TabPanel.propTypes = {
-    children: PropTypes.node,
-    index: PropTypes.any.isRequired,
-    value: PropTypes.any.isRequired,
-    tabId: PropTypes.string,
-};
 
 function a11yProps(index, classname = null) {
     const label = classname || index;
