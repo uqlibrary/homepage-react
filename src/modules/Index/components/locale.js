85195dbad2bb196aaee2c98de244433db4610673
Lea de Groot <l.degroot@uq.edu.au>
2020-10-26 11:54:50 +1000

    make PrimoSearch part of existing Shared Components [PT #174524208]

--- src/modules/Index/components/locale.js
+++ src/modules/Index/components/locale.js
@@ -2,7 +2,7 @@ import React from 'react';
 import AccessTimeIcon from '@material-ui/icons/AccessTime';
 import ImportantDevicesIcon from '@material-ui/icons/ImportantDevices';
 import InfoOutlined from '@material-ui/icons/InfoOutlined';
-import { primoSearch } from '../../reusable/PrimoSearch/components/primoSearchLocale';
+import { primoSearch } from 'modules/SharedComponents/PrimoSearch/components/primoSearchLocale';
 
 export default {
     PrimoSearch: primoSearch,
--- src/pages/CourseResources/components/SearchCourseResources.js
+++ src/pages/CourseResources/components/SearchCourseResources.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 
 import { TabPanel } from './TabPanel';
 
-import PrimoSearch from 'modules/reusable/PrimoSearch/containers/PrimoSearch';
+import PrimoSearch from 'modules/SharedComponents/PrimoSearch/containers/PrimoSearch';
 
 import AppBar from '@material-ui/core/AppBar';
 import Grid from '@material-ui/core/Grid';
