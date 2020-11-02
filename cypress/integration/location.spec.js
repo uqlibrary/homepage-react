13cad0afb261f9124e40d2a1071c9121a513f0ba
Ky Lane <k.lane@uq.edu.au>
2020-11-02 13:33:14 +1000

    [#175472137] quick refactor for Cypress tests

--- cypress/integration/location.spec.js
+++ cypress/integration/location.spec.js
@@ -19,35 +19,28 @@ context('Location component', () => {
         cy.getCookie('location').should('exist');
 
         cy.log('Should set the cookie to St Lucia');
-        cy.get('button[data-testid="location-computerAvailability-button"]')
-            .click();
+        cy.get('button[data-testid="location-computerAvailability-button"]').click();
         cy.get('li[data-testid="location-computerAvailability-option-1"').click();
         cy.getCookie('location').should('have.property', 'value', 'St%20Lucia');
-        cy.get('button[data-testid="location-computerAvailability-button"]')
-            .trigger('mouseover');
+        cy.get('button[data-testid="location-computerAvailability-button"]').trigger('mouseover');
         cy.get('div#location-computerAvailability-tooltip').contains('Current preferred location is St Lucia');
 
         cy.log('Should set the cookie to Gatton');
-        cy.get('button[data-testid="location-computerAvailability-button"]')
-            .click();
+        cy.get('button[data-testid="location-computerAvailability-button"]').click();
         cy.get('li[data-testid="location-computerAvailability-option-2"').click();
         cy.getCookie('location').should('have.property', 'value', 'Gatton');
-        cy.get('button[data-testid="location-computerAvailability-button"]')
-            .trigger('mouseover');
+        cy.get('button[data-testid="location-computerAvailability-button"]').trigger('mouseover');
         cy.get('div#location-computerAvailability-tooltip').contains('Current preferred location is Gatton');
 
         cy.log('Should set the cookie to Herston');
-        cy.get('button[data-testid="location-computerAvailability-button"]')
-            .click();
+        cy.get('button[data-testid="location-computerAvailability-button"]').click();
         cy.get('li[data-testid="location-computerAvailability-option-3"').click();
         cy.getCookie('location').should('have.property', 'value', 'Herston');
-        cy.get('button[data-testid="location-computerAvailability-button"]')
-            .trigger('mouseover');
+        cy.get('button[data-testid="location-computerAvailability-button"]').trigger('mouseover');
         cy.get('div#location-computerAvailability-tooltip').contains('Current preferred location is Herston');
 
         cy.log('Should set the cookie to null');
-        cy.get('button[data-testid="location-computerAvailability-button"]')
-            .click();
+        cy.get('button[data-testid="location-computerAvailability-button"]').click();
         cy.get('li[data-testid="location-computerAvailability-option-0"').click();
         cy.getCookie('location').should('have.property', 'value', 'null');
     });
