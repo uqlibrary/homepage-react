e6c4c2629b0d0096da0f8ffd9181a01c7c77acfb
Lea de Groot <l.degroot@uq.edu.au>
2020-11-04 15:29:20 +1000

    restrict guides by number [PT #174524208]

--- src/mock/data/records/libraryGuides_PHIL1002.js
+++ src/mock/data/records/libraryGuides_PHIL1002.js
@@ -1,4 +1,10 @@
 export default [{
     "title": "PHIL1002",
     "url": "https:\/\/guides.library.uq.edu.au\/phil1002",
+}, {
+    "title": "Philosophy",
+    "url": "https:\/\/guides.library.uq.edu.au\/Philosophy",
+}, {
+    "title": "Stuff",
+    "url": "https:\/\/guides.library.uq.edu.au\/somethingelse",
 }]
