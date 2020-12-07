17f78283ee3858d6fab96788aa2b84978032a15a
Lea de Groot <l.degroot@uq.edu.au>
2020-12-07 13:18:43 +1000

    fix the markup on the sample text [PT #175966168]

--- src/modules/Index/components/subComponents/promoPanel.locale.js
+++ src/modules/Index/components/subComponents/promoPanel.locale.js
@@ -3,15 +3,15 @@ import React from 'react';
 export const promoPanel = {
     title: 'Welcome to the Library!',
     content: (
-        <p>
-            Here are some useful links you might like:
+        <div>
+            <p>Here are some useful links you might like:</p>
             <ul>
                 <li>
                     <a href="/">A bullet point with a link</a>
                 </li>
                 <li>A second point that is just text.</li>
             </ul>
-            We hope you like our site.
-        </p>
+            <p>We hope you like our site.</p>
+        </div>
     ),
 };
