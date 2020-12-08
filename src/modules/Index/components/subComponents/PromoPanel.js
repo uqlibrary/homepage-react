d04215c2866606639fbfb738492fb55400d87843
Lea de Groot <l.degroot@uq.edu.au>
2020-12-09 08:56:48 +1000

    apply ellipsis to the promo panel title when the user supplies one that is too long [PT #175966168]

--- src/modules/Index/components/subComponents/PromoPanel.js
+++ src/modules/Index/components/subComponents/PromoPanel.js
@@ -4,6 +4,7 @@ import { PropTypes } from 'prop-types';
 import { promoPanel as locale } from './promoPanel.locale';
 
 import { StandardCard } from 'modules/SharedComponents/Toolbox/StandardCard';
+
 import Grid from '@material-ui/core/Grid';
 
 const PromoPanel = ({ account }) => {
@@ -14,13 +15,13 @@ const PromoPanel = ({ account }) => {
             standardCardId="promo-panel"
             title={
                 <Grid container>
-                    <Grid item xs>
+                    <Grid item xs={10} style={{ overflow: 'hidden', textOverflow: 'ellipsis' }}>
                         {!!account ? locale.loggedin.title : locale.loggedout.title}
                     </Grid>
                 </Grid>
             }
         >
-            <Grid container spacing={1} id="thingummy">
+            <Grid container spacing={1}>
                 <Grid item xs>
                     {!!account ? locale.loggedin.content : locale.loggedout.content}
                 </Grid>
