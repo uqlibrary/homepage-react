3af840b74f3575f4c179641c9636ec8623c457dd
Lea de Groot <l.degroot@uq.edu.au>
2020-12-09 12:59:46 +1000

    use latest implementation of Sentry [PT #176028459]

--- src/AppErrorBoundary.js
+++ src/AppErrorBoundary.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Raven from 'raven-js';
+import * as Sentry from '@sentry/react';
+import { Integrations } from '@sentry/tracing';
 
 class AppErrorBoundary extends React.Component {
     static propTypes = {
@@ -8,12 +10,24 @@ class AppErrorBoundary extends React.Component {
     };
 
     componentDidMount() {
-        if (process.env.ENABLE_LOG) {
-            Raven.config('https://88702b1b71434522871473c7e2490ed8@o136426.ingest.sentry.io/5379235', {
-                environment: process.env.BRANCH,
-                release: process.env.GIT_SHA,
-                whitelistUrls: [/library\.uq\.edu\.au/],
-            }).install();
+        if (
+            process.env.ENABLE_LOG &&
+            process.env.SENTRY_AUTH_TOKEN &&
+            process.env.SENTRY_ORG &&
+            process.env.SENTRY_PROJECT
+        ) {
+            Sentry.init({
+                dsn: `https://${process.env.SENTRY_AUTH_TOKEN}@${process.env.SENTRY_ORG}.ingest.sentry.io/${process.env.SENTRY_PROJECT}`,
+                integrations: [new Integrations.BrowserTracing()],
+
+                // We recommend adjusting this value in production, or using tracesSampler
+                // for finer control
+                tracesSampleRate: 1.0,
+
+                environment: process.env.BRANCH || 'branch undefined',
+                release: process.env.GIT_SHA || 'release undefined',
+                whitelistUrls: [/library\.uq\.edu\.au/], // no mention in current doc, may not work
+            });
         }
     }
 
