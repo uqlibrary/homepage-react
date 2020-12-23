10ec1b9dae0ea6651d386fa5fd2aacbcccc21d9b
Lea de Groot <l.degroot@uq.edu.au>
2020-12-24 09:31:04 +1000

    make the megamenu chevrons purple [PT #176045766]

--- src/modules/App/components/Megamenu.js
+++ src/modules/App/components/Megamenu.js
@@ -361,8 +361,8 @@ export function Megamenu(props) {
                         primary={menuItem.primaryText}
                         secondary={menuItem.secondaryText}
                     />
-                    {hasChildren && isSubMenuOpen[menuItem.id] && <ExpandLess size={iconSize} color="disabled" />}
-                    {hasChildren && !isSubMenuOpen[menuItem.id] && <ExpandMore size={iconSize} color="disabled" />}
+                    {hasChildren && isSubMenuOpen[menuItem.id] && <ExpandLess size={iconSize} color="primary" />}
+                    {hasChildren && !isSubMenuOpen[menuItem.id] && <ExpandMore size={iconSize} color="primary" />}
                 </ListItem>
                 {hasChildren && renderSubMenu(menuItem, index, classes)}
             </div>
