79ffa97b47686b17038bdbcc3ce54b02cbf1f831
Lea de Groot <l.degroot@uq.edu.au>
2021-01-06 18:47:09 +1000

    full coverage of ConnectFooter [PT #176027329]

--- src/modules/SharedComponents/Footer/components/ConnectFooter.test.js
+++ src/modules/SharedComponents/Footer/components/ConnectFooter.test.js
@@ -1,3 +1,9 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { default as locale } from '../footer.locale.js';
+import { ConnectFooter } from './ConnectFooter';
+
 Object.defineProperty(window, 'matchMedia', {
     writable: true,
     value: jest.fn().mockImplementation(query => ({
@@ -12,13 +18,16 @@ Object.defineProperty(window, 'matchMedia', {
     })),
 });
 
-import { ConnectFooter } from './ConnectFooter';
-
 function setup(testProps = {}) {
+    const history = {
+        push: jest.fn(),
+        go: jest.fn(),
+        location: { pathname: '/' },
+    };
     const props = {
         ...testProps,
         classes: {},
-        history: testProps.history || { push: jest.fn() },
+        history: testProps.history || history,
     };
     return getElement(ConnectFooter, props);
 }
@@ -28,5 +37,61 @@ describe('Component Megamenu', () => {
         const wrapper = setup();
         const tree = toJson(wrapper);
         expect(tree).toMatchSnapshot();
+    });
+
+    test('user can visit giving links', () => {
+        render(<ConnectFooter history={history} />);
+
+        const givingButton = [...locale.connectFooter.givingLinks].splice(0, 1).reduce(output => output);
+
+        const screenButton = screen.getByText(givingButton.label);
+
+        // from https://www.benmvp.com/blog/mocking-window-location-methods-jest-jsdom/
+        const oldWindowLocation = window.location;
+        delete window.location;
+
+        window.location = Object.defineProperties(
+            {},
+            {
+                ...Object.getOwnPropertyDescriptors(oldWindowLocation),
+                assign: {
+                    configurable: true,
+                    value: jest.fn(),
+                },
+            },
+        );
+        window.location.assign.mockReset();
+
+        userEvent.click(screenButton);
+
+        expect(window.location.assign).toHaveBeenCalledTimes(1);
+        expect(window.location.assign).toHaveBeenCalledWith(givingButton.linkTo);
+
+        // restore `window.location` to the `jsdom` `Location` object
+        window.location = oldWindowLocation;
+    });
+
+    test('user can visit social media links', () => {
+        render(<ConnectFooter history={history} />);
+
+        const socialButton = [...locale.connectFooter.buttonSocial].splice(0, 1).reduce(output => output);
+
+        // const screenButton = getByTestId(socialButton.dataTestid);
+        const screenButton = screen.getByTestId(socialButton.dataTestid);
+        // const screenButton = screen.getByTitle(socialButton.linkMouseOver);
+
+        const oldWindowOpen = window.open;
+        delete window.open;
+
+        window.open = jest.fn();
+        // window.open.assign.mockReset();
+
+        userEvent.click(screenButton);
+
+        expect(window.open).toHaveBeenCalledTimes(1);
+        expect(window.open).toHaveBeenCalledWith(socialButton.linkTo, undefined);
+
+        // restore `window.open`
+        window.open = oldWindowOpen;
     });
 });
