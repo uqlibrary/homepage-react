d5d4ef0792f9127fe5e2d71e38b2e4bd5338b614
uqjtilse <j.tilse@library.uq.edu.au>
2021-01-11 11:13:49 +1000

    Update promoPanel.locale.js
    Updated with prod copy for go live.

--- src/modules/Index/components/subComponents/promoPanel.locale.js
+++ src/modules/Index/components/subComponents/promoPanel.locale.js
@@ -6,32 +6,38 @@ const loginUrl = `${AUTH_URL_LOGIN}?url=${window.btoa(window.location.href)}`;
 
 export const promoPanel = {
     loggedout: {
-        title: 'New My Library!',
+        title: 'New home page!',
         content: (
             <div>
-                <p>Try the new My Library for a great personalised experience.</p>
+                <p>We have updated the home page with a great new look and feel.</p>    
+                <p>
+                    <a href={loginUrl}>Log in now</a> for a brand new personalised experience: 
+                </p>
                 <ul>
                     <li>Set your campus</li>
+                    <li>Top-up your print balance</li>        
                     <li>Get your course resources &amp; more</li>
-                    <li>Top-up your print balance</li>
                 </ul>
                 <p>
-                    <a href={loginUrl}>Log in now!</a>
+                    <a href="https://web.library.uq.edu.au/blog/2021/01/discover-new-library-home-page">Discover the new home page!</a>
                 </p>
             </div>
         ),
     },
     loggedin: {
-        title: 'New My Library!',
+        title: 'New home page!',
         content: (
             <div>
-                <p>Try the new My Library for a great personalised experience.</p>
-                <p>Depending on your user type, you will be able to:</p>
+                <p>We have personalised your home page just for you. Your page may contain additional features depdending on who you are.</p>
                 <ul>
-                    <li>Set your campus</li>
-                    <li>Get your course resources &amp; more</li>
+                    <li>Get your course resources (UQ students)</li>        
+                    <li>Set your campus to sort library hours &amp; computer availability</li>
                     <li>Top-up your print balance</li>
+                    <li>Click the My Library button for links to library services</li>
                 </ul>
+                <p>
+                    <a href="https://web.library.uq.edu.au/blog/2021/01/discover-new-library-home-page">More about your new home page!</a>
+                </p>
             </div>
         ),
     },
