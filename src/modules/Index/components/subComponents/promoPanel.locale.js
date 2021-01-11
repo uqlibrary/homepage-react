081df04ca808660af02af78e4d777469e68c8e94
Lea de Groot <l.degroot@uq.edu.au>
2021-01-11 11:34:57 +1000

    promo panel  syntax for Jake

--- src/modules/Index/components/subComponents/promoPanel.locale.js
+++ src/modules/Index/components/subComponents/promoPanel.locale.js
@@ -19,7 +19,9 @@ export const promoPanel = {
                     <li>Get your course resources &amp; more</li>
                 </ul>
                 <p>
-                    <a href="https://web.library.uq.edu.au/blog/2021/01/discover-new-library-home-page">Discover the new home page!</a>
+                    <a href="https://web.library.uq.edu.au/blog/2021/01/discover-new-library-home-page">
+                        Discover the new home page!
+                    </a>
                 </p>
             </div>
         ),
@@ -28,7 +30,10 @@ export const promoPanel = {
         title: 'New home page!',
         content: (
             <div>
-                <p>We have personalised your home page just for you. Your page may contain additional features depdending on who you are.</p>
+                <p>
+                    We have personalised your home page just for you. Your page may contain additional features
+                    depdending on who you are.
+                </p>
                 <ul>
                     <li>Get your course resources (UQ students)</li>
                     <li>Set your campus to sort library hours &amp; computer availability</li>
@@ -36,7 +41,9 @@ export const promoPanel = {
                     <li>Click the My Library button for links to library services</li>
                 </ul>
                 <p>
-                    <a href="https://web.library.uq.edu.au/blog/2021/01/discover-new-library-home-page">More about your new home page!</a>
+                    <a href="https://web.library.uq.edu.au/blog/2021/01/discover-new-library-home-page">
+                        More about your new home page!
+                    </a>
                 </p>
             </div>
         ),
