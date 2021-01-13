4b691831cdeec5589decfeb15fba0606a44a08f7
Ky Lane <k.lane@uq.edu.au>
2021-01-14 09:49:32 +1000

    Payment receipt page

---
+++ src/modules/Pages/PaymentReceipt/PaymentRecept.locale.js
@@ -0,0 +1,12 @@
+export default {
+    title: 'Payment receipt',
+    thanks: 'Thank you for using the UQ online payments system.',
+    transaction: 'Your transaction of $[AmountPaid] has been [Success].',
+    check: 'Please check your email for your receipt #[Receipt]',
+    maintenance:
+        'NOTE: Due to daily maintenance, if you top up your balance between 12AM and 1AM please allow up to 1 hour for funds to be deposited in your account.',
+    failed: {
+        title: 'Payment receipt data missing',
+        message: 'You have reached this page in error. Please go back, and check what brought you here.',
+    },
+};
