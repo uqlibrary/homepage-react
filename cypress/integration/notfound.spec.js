06e4e1ca206b3047777d71e986713b20b45599fe
Lea de Groot <l.degroot@uq.edu.au>
2021-02-09 13:53:16 +1000

    use 'data-testid should not exist' in preference to 'body should not contain' as the latter seems to fire false negatives [PT #176547487]

--- cypress/integration/notfound.spec.js
+++ cypress/integration/notfound.spec.js
@@ -4,24 +4,24 @@ context('authorisation errors', () => {
         // if we end up with other admin pages, swap this out
         cy.visit('/admin/masquerade/?user=s1111111');
         cy.viewport(1300, 1000);
-        cy.get('body').contains('The requested page could not be found.');
+        cy.get('[data-testid=page-not-found]').should('exist');
     });
     it('page that requires Admin does not return a not found error to privileged users', () => {
         // masquerade may be a special case, but we dont have a better example atm
         // if we end up with other admin pages, swap this out
         cy.visit('/admin/masquerade/?user=uqstaff');
         cy.viewport(1300, 1000);
-        cy.get('body').should('not.contain', 'The requested page could not be found.');
+        cy.get('[data-testid=page-not-found]').should('not.exist');
     });
     it('page that isnt available to all logged in users returns an authorisation error for non privileged users', () => {
         cy.visit('/courseresources?user=emcommunity');
         cy.viewport(1300, 1000);
-        cy.get('body').contains('The requested page is available to authorised users only.');
+        cy.get('[data-testid=permission-denied]').should('exist');
     });
     it('page that isnt available to all logged in users does not return an authorisation error for privileged users', () => {
         cy.visit('/courseresources?user=s1111111');
         cy.viewport(1300, 1000);
-        cy.get('body').should('not.contain', 'The requested page is available to authorised users only.');
+        cy.get('[data-testid=permission-denied]').should('not.exist');
     });
     it('page with an authorisation error has a working back button', () => {
         cy.visit('/?user=emcommunity'); // supply a page the back button can return to
@@ -37,12 +37,12 @@ context('authentication errors', () => {
     it('page that requires login returns an authentication error for non-loggedin user', () => {
         cy.visit('/courseresources?user=public');
         cy.viewport(1300, 1000);
-        cy.get('body').contains('The requested page is available to authenticated users only.');
+        cy.get('[data-testid=user-not-loggedin]').should('exist');
     });
     it('page that requires login does not return an authentication error for loggedin user', () => {
         cy.visit('/courseresources?user=uqstaff');
         cy.viewport(1300, 1000);
-        cy.get('body').should('not.contain', 'The requested page is available to authenticated users only.');
+        cy.get('[data-testid=user-not-loggedin]').should('not.exist');
     });
     it('page with an authentication error has a working back button', () => {
         cy.visit('/?user=public'); // supply a page the back button can return to
