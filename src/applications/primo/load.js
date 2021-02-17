093e22485c9e9b191efac2539f209a9f3b56bdca
Lea de Groot <l.degroot@uq.edu.au>
2021-02-18 08:37:55 +1000

    refactor primo load script [PT #176878294]

--- src/applications/primo/load.js
+++ src/applications/primo/load.js
@@ -6,6 +6,13 @@ function ready(fn) {
     }
 }
 
+/**
+ * the default homepage style blocks page scroll
+ */
+function resetDocumentScrollBar() {
+    document.body.style.overflow = 'auto';
+}
+
 function removeFooter() {
     const footerExists = setInterval(() => {
         const footer = document.getElementById('full-footer-block');
@@ -14,48 +21,56 @@ function removeFooter() {
 
             clearInterval(footerExists);
         }
-        document.body.style.overflow = 'auto'; // the default homepage style blocks page scroll
-    }, 100);
+    }, 300);
 }
 
+/**
+ * look for the homepage link ("Library" text) and the askus button
+ * move both into the primo login bar area
+ * then remove the library utility area
+ */
 function mergeUtilityAreaAndPrimoLoginBar() {
     let utilityCounter = 20; // avoid 'maximum call stack size exceeded' if element never becomes available
+
+    let homelinkComplete = false;
+    let askusComplete = false;
     const mergeAreas = setInterval(() => {
-        const homeLink = document.getElementById('uq-site-header-home-button');
-        if (!!homeLink) {
+        const homeLinkButton = document.getElementById('uq-site-header-home-button');
+        if (!!homeLinkButton && !homelinkComplete) {
             // work out where to make the home link sit
-            const box1 = (document.getElementsByTagName('md-content') || [])[0];
-            const box1Width = window.getComputedStyle(box1, null).getPropertyValue('padding-left');
+            const box1 = document.getElementsByTagName('md-content')[0] || false;
+            const box1Width = !!box1 ? window.getComputedStyle(box1, null).getPropertyValue('padding-left') : 0;
             const box2 = box1.firstChild.nextSibling;
-            const homeLinkLeft = box2.offsetWidth + parseInt(box1Width.replace('px', ''), 10);
+            const homeLinkLeft = (!!box2 ? box2.offsetWidth : 0) + parseInt(box1Width.replace('px', ''), 10);
 
-            const mainMenu = (document.getElementsByTagName('prm-main-menu') || [])[0];
-            mainMenu.insertBefore(homeLink, mainMenu.firstChild);
+            const mainMenu = document.getElementsByTagName('prm-main-menu')[0] || false;
+            mainMenu.insertBefore(homeLinkButton, mainMenu.firstChild);
 
-            homeLink.style.left = `${homeLinkLeft}px`;
-            homeLink.style.position = 'absolute';
+            !!homeLinkButton && (homeLinkButton.style.left = `${homeLinkLeft}px`);
+            !!homeLinkButton && (homeLinkButton.style.position = 'absolute');
+
+            homelinkComplete = true;
         }
 
         const askusButton = document.getElementById('askus-button-block');
-        if (!!askusButton) {
+        if (!!askusButton && !askusComplete) {
             const qrCodeScanner = document.getElementById('qrCodeScanner');
-            const parentDiv = qrCodeScanner.parentNode;
+            const parentDiv = !!qrCodeScanner && qrCodeScanner.parentNode;
             parentDiv.insertBefore(askusButton, qrCodeScanner);
+
+            askusComplete = true;
         }
 
-        if (!!homeLink && !!askusButton) {
+        if (!!homelinkComplete && !!askusComplete) {
             const nowEmptyHeader = document.getElementById('uq-site-header');
-            nowEmptyHeader.style.display = 'none';
+            !!nowEmptyHeader && (nowEmptyHeader.style.display = 'none');
         }
 
-        if ((!!homeLink && !!askusButton) || !utilityCounter) {
-            const nowEmptyHeader = document.getElementById('uq-site-header');
-            nowEmptyHeader.style.display = 'none';
-
+        if ((!!homelinkComplete && !!askusComplete) || !utilityCounter) {
             clearInterval(mergeAreas);
         }
         utilityCounter--;
-    }, 100); // check every 100ms
+    }, 300); // check every 100ms
 }
 
 function moveAlertsBelowPrimoLoginBar() {
@@ -64,22 +79,32 @@ function moveAlertsBelowPrimoLoginBar() {
         const libraryHeader = document.getElementById('content-container');
         const alertChild = document.getElementById('alert-container');
         if (!!alertChild && !!libraryHeader) {
-            const primoLogo = (document.getElementsByTagName('prm-logo') || [])[0];
-            const primoUtilityBar = primoLogo.parentNode;
-            primoUtilityBar.style.backgroundColor = '#ffffff';
+            // the primo uq logo is invisible, but still exist in the dom
+            // and can be used to get its parent, which doesnt have an id
+            const primoLogo = document.getElementsByTagName('prm-logo')[0] || false;
+            const primoUtilityBar = !!primoLogo && primoLogo.parentNode;
+            !!primoUtilityBar && (primoUtilityBar.style.backgroundColor = '#ffffff');
+
             const libraryHeader = document.getElementById('content-container');
-            const alertChild = document.getElementById('alert-container');
-            libraryHeader.insertBefore(primoUtilityBar, alertChild);
+            const alertsBlock = document.getElementById('alert-container');
+            !!libraryHeader &&
+                !!primoUtilityBar &&
+                !!alertsBlock &&
+                libraryHeader.insertBefore(primoUtilityBar, alertsBlock);
         }
         if ((!!alertChild && !!libraryHeader) || !alertsCounter) {
             clearInterval(moveAlertsArea);
         }
         alertsCounter--;
-    }, 100); // check every 100ms
+    }, 300); // check every 200ms
 }
 
 function loadReusableComponents() {
     // the react root is inserted via angular in the Primo custom.js file, within the Primo View Package
+
+    removeFooter();
+
+    resetDocumentScrollBar();
 
     mergeUtilityAreaAndPrimoLoginBar();
 
@@ -88,8 +113,6 @@ function loadReusableComponents() {
     // no auth button
 
     moveAlertsBelowPrimoLoginBar();
-
-    removeFooter();
 
     ready(loadReusableComponents);
 }
