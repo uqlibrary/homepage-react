b1e32733d282f0ae014e7b458b596c8721dcc44c
Lea de Groot <l.degroot@uq.edu.au>
2021-02-22 16:40:40 +1000

    make the training panel links able to be opened in a new window [PT #176449666]

--- src/modules/Index/components/subComponents/Training.js
+++ src/modules/Index/components/subComponents/Training.js
@@ -123,6 +123,37 @@ const useStyles = makeStyles(theme => ({
     actionButtons: {
         borderTopLeftRadius: 0,
         borderTopRightRadius: 0,
+        width: '100%',
+    },
+    actionButtonBlock: {
+        '& a': {
+            borderBottomLeftRadius: 4,
+            borderBottomRightRadius: 4,
+            color: '#fff',
+            display: 'block',
+            fontSize: 13,
+            fontWeight: 400,
+            padding: 6,
+            textAlign: 'center',
+            textTransform: 'uppercase',
+            width: '100%',
+            '&:hover': {
+                textDecoration: 'none',
+                boxShadow: 'none',
+            },
+        },
+    },
+    moreActionButton: {
+        backgroundColor: theme.palette.secondary.main,
+        '&:hover': {
+            backgroundColor: theme.palette.secondary.dark,
+        },
+    },
+    bookActionButton: {
+        backgroundColor: theme.palette.primary.main,
+        '&:hover': {
+            backgroundColor: theme.palette.primary.dark,
+        },
     },
     detailHeader: {
         backgroundColor: theme.palette.primary.dark,
@@ -159,12 +190,6 @@ const Training = ({ trainingEvents, trainingEventsLoading, trainingEventsError }
             document.getElementById(`training-event-detail-button-${entityId}`).focus();
         }, 300);
         setEventDetail(null);
-    };
-    const openEvent = id => {
-        window.location.href = `https://studenthub.uq.edu.au/students/events/detail/${id}`;
-    };
-    const openMoreTraining = () => {
-        window.location.href = 'https://web.library.uq.edu.au/library-services/training';
     };
     const eventTime = eventTime =>
         moment(eventTime)
@@ -324,35 +349,25 @@ const Training = ({ trainingEvents, trainingEventsLoading, trainingEventsError }
                 <div className={classes.flexFooter}>
                     {/* Buttons */}
                     <Grid container spacing={0}>
-                        <Grid item xs={12}>
+                        <Grid item xs={12} className={classes.actionButtonBlock}>
                             {!eventDetail ? (
-                                <Button
-                                    classes={{ root: classes.actionButtons }}
-                                    size="small"
-                                    variant="contained"
-                                    color="secondary"
-                                    disableElevation
-                                    fullWidth
-                                    onClick={() => openMoreTraining()}
+                                <a
+                                    className={classes.moreActionButton}
+                                    href="https://web.library.uq.edu.au/library-services/training"
                                     id="training-event-detail-more-training-button"
                                     data-testid="training-event-detail-more-training-button"
                                 >
                                     More training events
-                                </Button>
+                                </a>
                             ) : (
-                                <Button
-                                    classes={{ root: classes.actionButtons }}
-                                    size="small"
-                                    variant="contained"
-                                    color="primary"
-                                    disableElevation
-                                    fullWidth
-                                    onClick={() => openEvent(eventDetail.entityId)}
+                                <a
+                                    className={classes.bookActionButton}
+                                    href={`https://studenthub.uq.edu.au/students/events/detail/${eventDetail.entityId}`}
                                     id="training-event-detail-training-login-button"
                                     data-testid="training-event-detail-training-login-button"
                                 >
                                     Log in and book now
-                                </Button>
+                                </a>
                             )}
                         </Grid>
                     </Grid>
