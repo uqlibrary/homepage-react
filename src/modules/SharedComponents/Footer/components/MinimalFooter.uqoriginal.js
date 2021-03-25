310ab0d111e6d3c0df94cf360d513fd1c91e5dea
Ky Lane <k.lane@uq.edu.au>
2021-03-25 15:16:32 +1000

    Update tests

--- src/modules/SharedComponents/Footer/components/MinimalFooter.uqoriginal.js
+++ src/modules/SharedComponents/Footer/components/MinimalFooter.uqoriginal.js
@@ -12,38 +12,61 @@
  */
 <footer className="uq-footer" data-gtm-category="Footer">
     <div className="uq-footer__container">
-        <div className="uq-footer__acknowledgement"> UQ acknowledges the Traditional Owners and their custodianship of
-            the
-            lands on which UQ is situated. — <a href="https://about.uq.edu.au/reconciliation"
-                                                className="uq-footer__acknowledgement__link">Reconciliation
-                statement</a>
+        <div className="uq-footer__acknowledgement">
+            {' '}
+            UQ acknowledges the Traditional Owners and their custodianship of the lands on which UQ is situated. —{' '}
+            <a href="https://about.uq.edu.au/reconciliation" className="uq-footer__acknowledgement__link">
+                Reconciliation statement
+            </a>
         </div>
         <div className="uq-footer__row">
             <div className="uq-footer__column">
                 <div className="uq-footer__copyright">© The University of Queensland</div>
-                <div className="uq-footer__contact"> Enquiries: <a href="tel:+61733651111"
-                                                                   className="uq-footer__link footer__enquiries-phone"
-                                                                   onClick="gtag(&quot;event&quot;,&quot;click&quot;,{event_category:&quot;UQ Footer&quot;,event_label:&quot;Enquiries phone&quot;})"><span
-                    itemProp="telephone">+61 7 3365 1111</span></a> &nbsp; | &nbsp; <a
-                    href="https://uq.edu.au/contacts" className="uq-footer__link footer__contacts-link"
-                    onClick="gtag(&quot;event&quot;,&quot;click&quot;,{event_category:&quot;UQ Footer&quot;,event_label:&quot;Contact directory&quot;})">Contact
-                    directory</a></div>
-                <div className="uq-footer__meta"><abbr title="Australian Business Number">ABN</abbr>: 63 942 912
-                    684 &nbsp;
-                    | &nbsp; <abbr
-                        title="Commonwealth Register of Institutions and Courses for Overseas Students">CRICOS</abbr>
-                    Provider No: <a className="uq-footer__link cricos__link"
-                                    href="https://www.uq.edu.au/about/cricos-link"
-                                    rel="external"
-                                    onClick="gtag(&quot;event&quot;,&quot;click&quot;,{event_category:&quot;UQ Footer&quot;,event_label:&quot;CRICOS&quot;})">00025B</a>
+                <div className="uq-footer__contact">
+                    {' '}
+                    Enquiries:{' '}
+                    <a
+                        href="tel:+61733651111"
+                        className="uq-footer__link footer__enquiries-phone"
+                        onClick='gtag("event","click",{event_category:"UQ Footer",event_label:"Enquiries phone"})'
+                    >
+                        <span itemProp="telephone">+61 7 3365 1111</span>
+                    </a>{' '}
+                    &nbsp; | &nbsp;{' '}
+                    <a
+                        href="https://uq.edu.au/contacts"
+                        className="uq-footer__link footer__contacts-link"
+                        onClick='gtag("event","click",{event_category:"UQ Footer",event_label:"Contact directory"})'
+                    >
+                        Contact directory
+                    </a>
+                </div>
+                <div className="uq-footer__meta">
+                    <abbr title="Australian Business Number">ABN</abbr>: 63 942 912 684 &nbsp; | &nbsp;{' '}
+                    <abbr title="Commonwealth Register of Institutions and Courses for Overseas Students">CRICOS</abbr>
+                    Provider No:{' '}
+                    <a
+                        className="uq-footer__link cricos__link"
+                        href="https://www.uq.edu.au/about/cricos-link"
+                        rel="external"
+                        onClick='gtag("event","click",{event_category:"UQ Footer",event_label:"CRICOS"})'
+                    >
+                        00025B
+                    </a>
                 </div>
             </div>
             <div className="uq-footer__column">
-                <div className="uq-footer__emergency-contact uq-footer__aside"><strong
-                    className="uq-footer__sub-title">Emergency</strong><br />
-                    Phone: <a href="tel:+61733653333" className="uq-footer__link footer__emergency-phone"
-                              onClick="gtag(&quot;event&quot;,&quot;click&quot;,{event_category:&quot;UQ Footer&quot;,event_label:&quot;Emergency phone&quot;})">3365
-                    3333</a>
+                <div className="uq-footer__emergency-contact uq-footer__aside">
+                    <strong className="uq-footer__sub-title">Emergency</strong>
+                    <br />
+                    Phone:{' '}
+                    <a
+                        href="tel:+61733653333"
+                        className="uq-footer__link footer__emergency-phone"
+                        onClick='gtag("event","click",{event_category:"UQ Footer",event_label:"Emergency phone"})'
+                    >
+                        3365 3333
+                    </a>
                 </div>
             </div>
         </div>
@@ -52,25 +75,45 @@
                 <div className="uq-footer__column">
                     <div className="uq-footer__footer-menu">
                         <ul className="uq-footer__footer-menu__list">
-                            <li className="uq-footer__footer-menu__item"><a href="https://www.uq.edu.au/terms-of-use/"
-                                                                            rel="external"
-                                                                            className="uq-footer__footer-menu__link"
-                                                                            onClick="gtag(&quot;event&quot;,&quot;click&quot;,{event_category:&quot;UQ Footer&quot;,event_label:&quot;Privacy&quot;})">Privacy
-                                &amp; Terms of use</a></li>
-                            <li className="uq-footer__footer-menu__item"><a href="https://www.uq.edu.au/rti/"
-                                                                            rel="external"
-                                                                            className="uq-footer__footer-menu__link"
-                                                                            onClick="gtag(&quot;event&quot;,&quot;click&quot;,{event_category:&quot;UQ Footer&quot;,event_label:&quot;Right to Information&quot;})">Right
-                                to Information</a></li>
-                            <li className="uq-footer__footer-menu__item"><a href="https://uq.edu.au/accessibility/"
-                                                                            rel="external"
-                                                                            className="uq-footer__footer-menu__link"
-                                                                            onClick="gtag(&quot;event&quot;,&quot;click&quot;,{event_category:&quot;UQ Footer&quot;,event_label:&quot;Accessibility&quot;})">Accessibility</a>
+                            <li className="uq-footer__footer-menu__item">
+                                <a
+                                    href="https://www.uq.edu.au/terms-of-use/"
+                                    rel="external"
+                                    className="uq-footer__footer-menu__link"
+                                    onClick='gtag("event","click",{event_category:"UQ Footer",event_label:"Privacy"})'
+                                >
+                                    Privacy &amp; Terms of use
+                                </a>
                             </li>
-                            <li className="uq-footer__footer-menu__item"><a
-                                href="https://its.uq.edu.au/feedback?r=https://uq.edu.au" rel="external"
-                                className="uq-footer__footer-menu__link"
-                                onClick="gtag(&quot;event&quot;,&quot;click&quot;,{event_category:&quot;UQ Footer&quot;,event_label:&quot;Feedback&quot;})">Feedback</a>
+                            <li className="uq-footer__footer-menu__item">
+                                <a
+                                    href="https://www.uq.edu.au/rti/"
+                                    rel="external"
+                                    className="uq-footer__footer-menu__link"
+                                    onClick='gtag("event","click",{event_category:"UQ Footer",event_label:"Right to Information"})'
+                                >
+                                    Right to Information
+                                </a>
+                            </li>
+                            <li className="uq-footer__footer-menu__item">
+                                <a
+                                    href="https://uq.edu.au/accessibility/"
+                                    rel="external"
+                                    className="uq-footer__footer-menu__link"
+                                    onClick='gtag("event","click",{event_category:"UQ Footer",event_label:"Accessibility"})'
+                                >
+                                    Accessibility
+                                </a>
+                            </li>
+                            <li className="uq-footer__footer-menu__item">
+                                <a
+                                    href="https://its.uq.edu.au/feedback?r=https://uq.edu.au"
+                                    rel="external"
+                                    className="uq-footer__footer-menu__link"
+                                    onClick='gtag("event","click",{event_category:"UQ Footer",event_label:"Feedback"})'
+                                >
+                                    Feedback
+                                </a>
                             </li>
                         </ul>
                     </div>
@@ -78,4 +121,4 @@
             </div>
         </div>
     </div>
-</footer>
+</footer>;
