f0be60295dea77309b83f6eb70aa6320ae1f58b6
Lea de Groot <l.degroot@uq.edu.au>
2021-04-08 15:46:41 +1000

    tweak include of reusable so you can use an aws branch in local development [PT #177655781]

--- public/includes.js
+++ public/includes.js
@@ -17,22 +17,31 @@ function insertScript(url) {
 
 /*
  * valid paths:
- * http://localhost:2020/ will load https://localhost:8080/
+ * http://localhost:2020/ will load https://localhost:8080/uq-lib-reusable.min.js
+ * http://localhost:2020/?reusablebranch=master will load https://assets.library.uq.edu.au/reusable-webcomponents-development/master/uq-lib-reusable.min.js
  * https://homepage-development.library.uq.edu.au/feature-drupal/#/ will load https://assets.library.uq.edu.au/reusable-webcomponents-development/feature-drupal/uq-lib-reusable.min.js
- * https://homepage-development.library.uq.edu.au/feature-leadegroot-1/#/?branch=primo-prod-dev will load https://assets.library.uq.edu.au/reusable-webcomponents-development/primo-prod-dev/uq-lib-reusable.min.js
+ * https://homepage-development.library.uq.edu.au/feature-leadegroot-1/#/?reusablebranch=primo-prod-dev will load https://assets.library.uq.edu.au/reusable-webcomponents-development/primo-prod-dev/uq-lib-reusable.min.js
  * https://homepage-staging.library.uq.edu.au/ loads https://assets.library.uq.edu.au/reusable-webcomponents-staging/uq-lib-reusable.min.js
  * https://www.library.uq.edu.au/ loads https://assets.library.uq.edu.au/reusable-webcomponents/uq-lib-reusable.min.js
  */
 
 const isLocal = window.location.hostname === 'localhost';
-const localPath = 'http://localhost:8080/uq-lib-reusable.min.js';
 
 const isDev = window.location.hostname === 'homepage-development.library.uq.edu.au';
 // list of valid branches found in reusable-webcomponents AWS pipeline deployments (if not in AWS you will get a 404)
-const branchList = ['feature-leadegroot', 'feature-drupal', 'primo-prod-dev', 'primo-sandbox', 'primo-sandbox-dev'];
+const branchList = [
+    'feature-leadegroot',
+    'feature-drupal',
+    'master',
+    'primo-prod-dev',
+    'primo-sandbox',
+    'primo-sandbox-dev',
+    'production',
+    'staging',
+];
 const devBranch = branchList.filter(branchName => {
-    const regex = new RegExp(`branch=${branchName}`);
-    return window.location.pathname.startsWith(`/${branchName}`) || regex.test(window.location.search);
+    const regex = new RegExp(`reusablebranch=${branchName}`);
+    return window.location.pathname.startsWith(`/${branchName}/`) || regex.test(window.location.search);
 });
 const branch = devBranch.length === 1 ? devBranch.pop() : 'master';
 
@@ -41,11 +50,16 @@ const devPath = `reusable-webcomponents-development/${branch}`;
 const isStaging = window.location.hostname === 'homepage-staging.library.uq.edu.au';
 const stagingPath = 'reusable-webcomponents-staging';
 
-const prodPath = 'reusable-webcomponents-production';
+const prodPath = 'reusable-webcomponents';
 
 // eslint-disable-next-line no-nested-ternary
 const path = isDev ? devPath : isStaging ? stagingPath : prodPath;
 const assetRoot = 'https://assets.library.uq.edu.au';
 
-const scriptUrl = isLocal ? localPath : `${assetRoot}/${path}/uq-lib-reusable.min.js`;
+const remoteFile = `${assetRoot}/${path}/uq-lib-reusable.min.js`;
+const localFile = new RegExp('reusablebranch=').test(window.location.search)
+    ? `${assetRoot}/${devPath}/uq-lib-reusable.min.js`
+    : 'http://localhost:8080/uq-lib-reusable.min.js';
+
+const scriptUrl = isLocal ? localFile : remoteFile;
 insertScript(scriptUrl);
