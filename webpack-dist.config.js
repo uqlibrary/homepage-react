a3aea11d43584add36a34f6afccc2d9141d5f4a9
Ky <k.lane@uq.edu.au>
2021-04-20 14:25:46 +1000

    remove entry from webpack

--- webpack-dist.config.js
+++ webpack-dist.config.js
@@ -59,7 +59,7 @@ const webpackConfig = {
     // The entry file. All your app roots from here.
     entry: {
         browserUpdate: join(__dirname, 'public', 'browser-update.js'),
-        includes: join(__dirname, 'public', 'includes.js'),
+        // includes: join(__dirname, 'public', 'includes.js'),
         main: resolve(__dirname, './src/index.js'),
         vendor: ['react', 'react-dom', 'react-router-dom', 'redux', 'react-redux', 'moment'],
     },
