42f0e50a44ecf10d1bf2f5780e8c979c2e670e77
Lea de Groot <l.degroot@uq.edu.au>
2021-05-18 08:28:15 +1000

    cleanup [PT #176490543]

--- src/actions/secureCollectionAction.js
+++ src/actions/secureCollectionAction.js
@@ -7,8 +7,7 @@ export function loadSecureCollectionCheck(path) {
     console.log('start of action:loadSecureCollectionCheck for ', path);
     return dispatch => {
         dispatch({ type: actions.SECURE_COLLECTION_CHECK_LOADING });
-        const api = SECURE_COLLECTION_CHECK_API({ path });
-        return get(api)
+        return get(SECURE_COLLECTION_CHECK_API({ path }))
             .then(data => {
                 dispatch({
                     type: actions.SECURE_COLLECTION_CHECK_LOADED,
@@ -34,8 +33,7 @@ export function loadSecureCollectionFile(path) {
     console.log('start of action: will load Secure Collection File for ', path);
     return dispatch => {
         dispatch({ type: actions.SECURE_COLLECTION_CHECK_LOADING });
-        const api = SECURE_COLLECTION_API({ path });
-        return get(api)
+        return get(SECURE_COLLECTION_API({ path }))
             .then(data => {
                 dispatch({
                     type: actions.SECURE_COLLECTION_CHECK_LOADED,
