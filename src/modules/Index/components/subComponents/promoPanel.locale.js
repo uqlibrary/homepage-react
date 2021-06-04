fe03f643f3d344bdcbb140cab385fa81497d3bff
Lea de Groot <l.degroot@uq.edu.au>
2021-06-04 10:09:50 +1000

    Fix code formatting for Jake's word changes

--- src/modules/Index/components/subComponents/promoPanel.locale.js
+++ src/modules/Index/components/subComponents/promoPanel.locale.js
@@ -1,8 +1,7 @@
 import React from 'react';
 
-import { AUTH_URL_LOGIN } from 'config';
-
-const loginUrl = `${AUTH_URL_LOGIN}?return=${window.btoa(window.location.href)}`;
+// import { AUTH_URL_LOGIN } from 'config';
+// const loginUrl = `${AUTH_URL_LOGIN}?return=${window.btoa(window.location.href)}`;
 
 export const promoPanel = {
     loggedout: {
@@ -10,16 +9,12 @@ export const promoPanel = {
         content: (
             <div>
                 <p>
-                    <a href="https://web.library.uq.edu.au/contact-us">
-                        Contact us 
-                    </a> 
-                    for online exam technical support. The service is available 7am-9pm on exam days between 5 and 18 June. 
+                    <a href="https://web.library.uq.edu.au/contact-us">Contact us</a> for online exam technical support.
+                    The service is available 7am-9pm on exam days between 5 and 18 June.
                 </p>
                 <p>
-                    <a href="https://life.uq.edu.au/study-spaces">
-                        Study spaces
-                    </a> 
-                    are available in the Library and on-campus. 
+                    <a href="https://life.uq.edu.au/study-spaces">Study spaces</a> are available in the Library and
+                    on-campus.
                 </p>
                 <p>Good luck with your exams!</p>
             </div>
@@ -30,16 +25,12 @@ export const promoPanel = {
         content: (
             <div>
                 <p>
-                    <a href="https://web.library.uq.edu.au/contact-us">
-                        Contact us 
-                    </a> 
-                    for online exam technical support. The service is available 7am-9pm on exam days between 5 and 18 June. 
+                    <a href="https://web.library.uq.edu.au/contact-us">Contact us</a> for online exam technical support.
+                    The service is available 7am-9pm on exam days between 5 and 18 June.
                 </p>
                 <p>
-                    <a href="https://life.uq.edu.au/study-spaces">
-                        Study spaces
-                    </a> 
-                    are available in the Library and on-campus. 
+                    <a href="https://life.uq.edu.au/study-spaces">Study spaces</a> are available in the Library and
+                    on-campus.
                 </p>
                 <p>Good luck with your exams!</p>
             </div>
