8a324220fb3cb2647372ff5c5149477118d80d5b
Lea de Groot <l.degroot@uq.edu.au>
2021-07-30 13:15:04 +1000

    interface for Clone Alert [PT #178735927]

--- cypress/integration/spotlights.spec.js
+++ cypress/integration/spotlights.spec.js
@@ -1,3 +1,22 @@
+context('ACCESSIBILITY', () => {
+    it('Spotlights', () => {
+        cy.visit('/');
+        cy.injectAxe();
+        cy.viewport(1300, 1000);
+        cy.log('Spootlights');
+        cy.get('img[alt="Academic Integrity Modules - Everything you need to know about academic integrity at UQ"]')
+            .should('be.visible')
+            .and($img => {
+                expect($img[0].naturalWidth).to.be.greaterThan(0);
+            });
+        // TODO: Does not current pass
+        cy.checkA11y('div[data-testid="spotlights"]', {
+            reportName: 'Spotlights',
+            scopeName: 'As loaded',
+            includedImpacts: ['minor', 'moderate', 'serious', 'critical'],
+        });
+    });
+});
 context('Spotlights', () => {
     it('Renders as expected', () => {
         cy.visit('/');
