cc5e6dfb0cd3cd806365a126775115cb1ac550ca
uqjtilse <j.tilse@library.uq.edu.au>
2021-08-04 13:46:24 +1000

    Updated formatting and box title.

--- src/modules/Index/components/subComponents/promoPanel.locale.js
+++ src/modules/Index/components/subComponents/promoPanel.locale.js
@@ -7,30 +7,30 @@ import React from 'react';
 
 export const promoPanel = {
     loggedout: {
-        title: 'We’re available online!',
+        title: 'Open online!',
         content: (
             <div>
-                <p>We’re online to help you. Access the Library from home, including:</p>
+                <p>We are online to help you. Access the Library from home, including:</p>
                 <ul>
-                    <li><a href="https://web.library.uq.edu.au/library-services/students/course-reading-lists">Reading lists</a>{' '}for your courses</li>
-                    <li><a href="https://guides.library.uq.edu.au/subject">Subject guides</a>{' '}for specialised resources in your subject area</li>
-                    <li>Tips for{' '}<a href="https://web.library.uq.edu.au/library-services/students/how-study-online">studying online</a>.</li>
+                    <li><a href="https://web.library.uq.edu.au/library-services/students/course-reading-lists">Reading lists</a> for your courses</li>
+                    <li><a href="https://guides.library.uq.edu.au/subject">Subject guides</a> for specialised resources in your subject area</li>
+                    <li>Tips for <a href="https://web.library.uq.edu.au/library-services/students/how-study-online">studying online</a>.</li>
                 </ul>
-                <p><a href="https://web.library.uq.edu.au/contact-us">Contact us</a>{' '}, including on chat or by phone during business hours, if you need assistance.</p>
+                <p><a href="https://web.library.uq.edu.au/contact-us">Contact us</a>, including on chat or by phone during business hours, if you need assistance.</p>
             </div>
         ),
     },
     loggedin: {
-        title: 'We’re available online',
+        title: 'Open online!',
         content: (
             <div>
-                <p>We’re online to help you. Access the Library from home, including:</p>
+                <p>We are online to help you. Access the Library from home, including:</p>
                 <ul>
-                    <li><a href="https://web.library.uq.edu.au/library-services/students/course-reading-lists">Reading lists</a>{' '}for your courses</li>
-                    <li><a href="https://guides.library.uq.edu.au/subject">Subject guides</a>{' '}for specialised resources in your subject area</li>
-                    <li>Tips for{' '}<a href="https://web.library.uq.edu.au/library-services/students/how-study-online">studying online</a>.</li>
+                    <li><a href="https://web.library.uq.edu.au/library-services/students/course-reading-lists">Reading lists</a> for your courses</li>
+                    <li><a href="https://guides.library.uq.edu.au/subject">Subject guides</a> for specialised resources in your subject area</li>
+                    <li>Tips for <a href="https://web.library.uq.edu.au/library-services/students/how-study-online">studying online</a>.</li>
                 </ul>
-                <p><a href="https://web.library.uq.edu.au/contact-us">Contact us</a>{' '}, including on chat or by phone during business hours, if you need assistance.</p>
+                <p><a href="https://web.library.uq.edu.au/contact-us">Contact us</a>, including on chat or by phone during business hours, if you need assistance.</p>
             </div>
         ),
     },
