8937caddceef9c7e6880046adec870c63d5ece4c
Lea de Groot <l.degroot@uq.edu.au>
2021-08-06 14:57:10 +1000

    make the mock data on the get record match the list entry that clicks to it [PT #179112921]

--- src/mock/index.js
+++ src/mock/index.js
@@ -192,8 +192,8 @@ mock.onGet(routes.ALERT_BY_ID_API({ id: '1db618c0-d897-11eb-a27e-df4e46db7245' }
         200,
         {
             id: '1db618c0-d897-11eb-a27e-df4e46db7245',
-            start: '2020-06-07 02:00:03',
-            end: '2020-06-07 03:00:03',
+            start: '2021-06-29 15:00:34',
+            end: '2031-07-02 18:30:34',
             title: 'Example alert:',
             body:
                 'This alert can be edited in mock.[permanent][UQ community COVID-19 advice](https://about.uq.edu.au/coronavirus)',
