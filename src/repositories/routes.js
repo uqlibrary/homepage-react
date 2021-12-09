e0f29b2684a42beddcd2593153a72728cb1e8b99
Lea de Groot <l.degroot@uq.edu.au>
2021-12-09 16:24:14 +1000

    remove primo code - search portal now in reusable repo [PT #180551430]

--- src/repositories/routes.js
+++ src/repositories/routes.js
@@ -61,21 +61,6 @@ export const INCOMPLETE_NTRO_RECORDS_API = () => ({
     },
 });
 
-// Primo Suggestions API
-// https://primo-instant-apac.hosted.exlibrisgroup.com/solr/ac?q=cats&facet=off&fq=scope%3A()%2BAND%2Bcontext%3A(B)&rows=10&wt=json&json.wrf=byutv_jsonp_callback_c631f96adec14320b23f1cac342d30f6&_=2ef82775b72140a6bde04ea6e20012e4
-export const PRIMO_SUGGESTIONS_API_GENERIC = ({ keyword }) => {
-    return {
-        apiUrl:
-            'https://primo-instant-apac.hosted.exlibrisgroup.com/solr/ac?q=' +
-            keyword +
-            '&facet=off' +
-            '&fq=scope%3A()%2BAND%2Bcontext%3A(B)' +
-            '&rows=10' +
-            '&wt=json' +
-            '&json.wrf=byutv_jsonp_callback_c631f96adec14320b23f1cac342d30f6',
-    };
-};
-
 export const PRIMO_SUGGESTIONS_API_EXAMS = ({ keyword }) => ({
     apiUrl: 'https://api.library.uq.edu.au/v1/search_suggestions?type=exam_paper&prefix=' + keyword,
 });
