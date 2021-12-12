02dfdbcfad68b97c8257d6f0263fd1f56a64500f
Lea de Groot <l.degroot@uq.edu.au>
2021-12-13 08:55:46 +1000

    fuller testing for homepage Computers [PT #180551430]

--- cypress/integration/homepage/computers.spec.js
+++ cypress/integration/homepage/computers.spec.js
@@ -1,5 +1,5 @@
-context('Computer availability Accessibility', () => {
-    it('Computer availability', () => {
+context('Computer availability ', () => {
+    it('Computer availability is accessible', () => {
         cy.visit('/');
         cy.injectAxe();
         cy.viewport(1300, 1000);
@@ -27,8 +27,73 @@ context('Computer availability Accessibility', () => {
         cy.wait(500);
         cy.checkA11y('div[data-testid="computers-library-dialog"]', {
             reportName: 'Computers',
-            scopeName: 'Lvel map',
+            scopeName: 'Level map',
             includedImpacts: ['minor', 'moderate', 'serious', 'critical'],
         });
+    });
+    it('has appropriate aria-labels', () => {
+        cy.visit('/');
+        cy.viewport(1300, 1000);
+        cy.get('button[data-testid="computers-library-button-1"]').contains('Biological Sciences Library');
+        cy.get('button[data-testid="computers-library-button-1"]')
+            .should('have.attr', 'aria-label')
+            .then(ariaLabel => {
+                expect(ariaLabel).to.contains('Biological Sciences Library - 108 free of');
+                expect(ariaLabel).to.contains('269. Click to review each level');
+            });
+        cy.get('button[data-testid="computers-library-button-1"]').click();
+
+        cy.get('[data-testid="computers-library-1-level-4-button"]').contains('Level 4');
+        cy.get('[data-testid="computers-library-1-level-4-button"]')
+            .should('have.attr', 'aria-label')
+            .then(ariaLabel => {
+                expect(ariaLabel).to.contains('Biological Sciences Library level 4. 72 free of');
+                expect(ariaLabel).to.contains('110');
+                expect(ariaLabel).to.contains('computers');
+            });
+    });
+});
+context('Minor functionality works', () => {
+    it('the display label has the correct number of computers free', () => {
+        cy.visit('/');
+        cy.viewport(1300, 1000);
+        cy.get('button[data-testid="computers-library-button-1"]').contains('Biological Sciences Library');
+        cy.get('button[data-testid="computers-library-button-1"]').click();
+
+        cy.get('[data-testid="computers-library-1-level-4-button"]').contains('Level 4');
+        cy.get('[data-testid="computers-library-1-level-4-button"]')
+            .parent()
+            .parent()
+            .find('div:nth-child(3)')
+            .contains('72')
+            .contains(' free of')
+            .contains('110');
+    });
+    it('close options for map and levels works', () => {
+        cy.visit('/');
+        cy.viewport(1300, 1000);
+        cy.get('button[data-testid="computers-library-button-1"]')
+            .should('exist')
+            .contains('Biological Sciences Library')
+            .click();
+        // levels list opens
+        cy.get('[data-testid="computers-library-1-level-4-button"]')
+            .should('exist')
+            .contains('Level 4')
+            .click();
+        // map loads
+        cy.get('h3')
+            .should('exist')
+            .contains('Biological Sciences Library');
+        // close map
+        cy.get('[data-testid="computers-library-dialog-close-button"]').click();
+        cy.get('h3').should('not.exist');
+        // level list is open, click to close
+        cy.get('[data-testid="computers-library-1-level-4-button"]').should('exist');
+        cy.get('button[data-testid="computers-library-button-1"]').click();
+        cy.get('[data-testid="computers-library-1-level-4-button"]').should('not.exist');
+        cy.get('button[data-testid="computers-library-button-1"]')
+            .should('exist')
+            .contains('Biological Sciences Library');
     });
 });
