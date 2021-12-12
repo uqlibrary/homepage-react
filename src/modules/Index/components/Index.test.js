c01a9109bf98390f9f64669b1a00bd34549cfedc
Lea de Groot <l.degroot@uq.edu.au>
2021-12-13 09:44:29 +1000

    add course resources unit tests for coverage [PT #180551430]

--- src/modules/Index/components/Index.test.js
+++ src/modules/Index/components/Index.test.js
@@ -1,4 +1,5 @@
 import Index from './Index';
+import { getUrlForCourseResourceSpecificTab } from './subComponents/CourseResourcesPanel';
 
 function setup(testProps = {}, args = { isShallow: false }) {
     const props = {
@@ -11,5 +12,31 @@ describe('Index page', () => {
     it('should render placeholders', () => {
         const wrapper = setup();
         expect(toJson(wrapper)).toMatchSnapshot();
+    });
+});
+
+describe('Course Resources panel', () => {
+    it('something', () => {
+        const aclass = {
+            ACAD_CAREER: 'UGRD',
+            ACAD_GROUP: 'SCI',
+            CAMPUS: 'STLUC',
+            CATALOG_NBR: '1201',
+            DESCR: 'The Australian Experience',
+            INSTRUCTION_MODE: 'IN',
+            STRM: '7060',
+            SUBJECT: 'HIST',
+            classnumber: 'HIST1201',
+            semester: 'Semester 2 2020',
+        };
+        const page = {
+            search: '?keyword',
+        };
+        expect(getUrlForCourseResourceSpecificTab(aclass, page)).toEqual(
+            '/courseresources?keyword&coursecode=HIST1201&campus=St Lucia&semester=Semester 2 2020',
+        );
+        expect(getUrlForCourseResourceSpecificTab(aclass, page, true, true)).toEqual(
+            'http://localhost/courseresources?keyword&coursecode=HIST1201&campus=undefined&semester=Semester 2 2020',
+        );
     });
 });
