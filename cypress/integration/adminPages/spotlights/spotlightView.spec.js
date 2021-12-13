21b5f98b75086c7ec8089a6c63cd0b6e753252ed
Lea de Groot <l.degroot@uq.edu.au>
2021-12-13 13:17:54 +1000

    add clickbutton to spotlightview to make it more reliable [PT #180551430]

--- cypress/integration/adminPages/spotlights/spotlightView.spec.js
+++ cypress/integration/adminPages/spotlights/spotlightView.spec.js
@@ -3,6 +3,7 @@ import {
     hasAWorkingHelpButton,
     numberCurrentPublishedSpotlights,
 } from '../../../support/spotlights';
+import { clickButton } from '../../../support/helpers';
 
 describe('Spotlight Admin View page', () => {
     before(() => {
@@ -67,7 +68,7 @@ describe('Spotlight Admin View page', () => {
     });
     it('can visit the clone page from the view page', () => {
         cy.wait(50);
-        cy.get('button[data-testid="admin-spotlights-form-button-save"]').click();
+        clickButton('button[data-testid="admin-spotlights-form-button-save"]');
         cy.location('href').should(
             'eq',
             'http://localhost:2020/admin/spotlights/clone/1e1b0e10-c400-11e6-a8f0-47525a49f469',
