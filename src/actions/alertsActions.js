8e1c9efa84c49a78d7b273ccf4e1cb45987c786c
Lea de Groot <l.degroot@uq.edu.au>
2022-04-14 14:02:51 +1000

    cleanup [PT #181397797]

--- src/actions/alertsActions.js
+++ src/actions/alertsActions.js
@@ -34,7 +34,6 @@ export function loadAllAlerts() {
 }
 
 export const createAlert = request => {
-    console.log('createAlert', request);
     return async dispatch => {
         dispatch({ type: actions.ALERT_LOADING });
         return post(ALERT_CREATE_API(), request)
@@ -54,7 +53,6 @@ export const createAlert = request => {
 };
 
 export const saveAlertChange = request => {
-    console.log('saveAlertChange: ', request);
     return async dispatch => {
         dispatch({ type: actions.ALERT_LOADING });
         return post(ALERT_SAVE_API({ id: request.id }), request)
