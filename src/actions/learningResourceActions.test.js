9d06c637b332f644e4f330733d98145588403a5c
Lea de Groot <l.degroot@uq.edu.au>
2022-04-26 12:57:00 +1000

    cleanup & remove unused tests [PT #181722246]

--- src/actions/learningResourceActions.test.js
+++ src/actions/learningResourceActions.test.js
@@ -12,7 +12,6 @@ import {
     loadGuides,
     loadReadingLists,
 } from './learningResourceActions';
-import { accounts } from '../mock/data';
 
 jest.mock('raven-js');
 
@@ -31,156 +30,165 @@ describe('Account action creators', () => {
         mockSessionApi.reset();
     });
 
-    it('dispatches expected actions when possible guides call fails', async () => {
-        mockApi.onGet(repositories.routes.GUIDES_API({ keyword: 'FREN1010' })).reply(500);
+    describe('Guides actions', () => {
+        it('dispatches expected actions when possible guides call fails', async () => {
+            mockApi.onGet(repositories.routes.GUIDES_API({ keyword: 'FREN1010' })).reply(500);
 
-        const expectedActions = [actions.GUIDES_LOADING, actions.GUIDES_FAILED];
+            const expectedActions = [actions.GUIDES_LOADING, actions.GUIDES_FAILED];
 
-        await mockActionsStore.dispatch(loadGuides('FREN1010'));
-        expect(mockActionsStore.getActions()).toHaveDispatchedActions(expectedActions);
+            await mockActionsStore.dispatch(loadGuides('FREN1010'));
+            expect(mockActionsStore.getActions()).toHaveDispatchedActions(expectedActions);
+        });
+
+        it('should dispatch clear guides list action', async () => {
+            const expectedActions = [actions.GUIDES_CLEAR];
+
+            await mockActionsStore.dispatch(clearGuides());
+            expect(mockActionsStore.getActions()).toHaveDispatchedActions(expectedActions);
+        });
+
+        it('handles a guides get request', async () => {
+            mockApi.onGet(repositories.routes.GUIDES_API({ keyword: 'FREN1010' }).apiUrl).reply(200, [{}]);
+
+            const expectedActions = [actions.GUIDES_LOADING, actions.GUIDES_LOADED];
+
+            await mockActionsStore.dispatch(loadGuides('FREN1010'));
+            expect(mockActionsStore.getActions()).toHaveDispatchedActions(expectedActions);
+        });
     });
 
-    it('should dispatch clear guides list action', async () => {
-        const expectedActions = [actions.GUIDES_CLEAR];
+    describe('Exams actions', () => {
+        it('dispatches expected actions when possible exams call fails', async () => {
+            mockApi.onGet(repositories.routes.LEARNING_RESOURCES_EXAMS_API({ keyword: 'FREN1010' })).reply(500);
 
-        await mockActionsStore.dispatch(clearGuides());
-        expect(mockActionsStore.getActions()).toHaveDispatchedActions(expectedActions);
+            const expectedActions = [actions.EXAMS_LEARNING_RESOURCES_LOADING, actions.EXAMS_LEARNING_RESOURCES_FAILED];
+
+            await mockActionsStore.dispatch(loadExamLearningResources('FREN1010'));
+            expect(mockActionsStore.getActions()).toHaveDispatchedActions(expectedActions);
+        });
+
+        it('should dispatch clear exams list action', async () => {
+            const expectedActions = [actions.EXAMS_LEARNING_RESOURCES_CLEAR];
+
+            await mockActionsStore.dispatch(clearExamLearningResources());
+            expect(mockActionsStore.getActions()).toHaveDispatchedActions(expectedActions);
+        });
+
+        it('handles an exams get request', async () => {
+            mockApi
+                .onGet(repositories.routes.LEARNING_RESOURCES_EXAMS_API({ keyword: 'FREN1010' }).apiUrl)
+                .reply(200, {});
+
+            const expectedActions = [actions.EXAMS_LEARNING_RESOURCES_LOADING, actions.EXAMS_LEARNING_RESOURCES_LOADED];
+
+            await mockActionsStore.dispatch(loadExamLearningResources('FREN1010'));
+            expect(mockActionsStore.getActions()).toHaveDispatchedActions(expectedActions);
+        });
     });
 
-    it('handles a guides get request', async () => {
-        mockApi.onGet(repositories.routes.GUIDES_API({ keyword: 'FREN1010' }).apiUrl).reply(200, [{}]);
+    describe('Reading lists actions', () => {
+        // should never happen
+        it('handles an empty reading list', async () => {
+            const emptyReadingList = {
+                title: 'FREN1010',
+                course_title: 'Introductory French 1',
+                reading_lists: [
+                    {
+                        campus: 'St Lucia',
+                        url: 'http://lr.library.uq.edu.au/lists/162A2ECA-2921-8A7C-CE27-C0B38055B9D7',
+                        period: 'Semester 2 2020',
+                        list: [],
+                    },
+                ],
+            };
+            mockApi
+                .onGet(
+                    repositories.routes.READING_LIST_API({
+                        coursecode: 'FREN1010',
+                        campus: 'St Lucia',
+                        semester: 'Semester 2 2020',
+                    }).apiUrl,
+                )
+                .reply(200, emptyReadingList);
 
-        const expectedActions = [actions.GUIDES_LOADING, actions.GUIDES_LOADED];
+            const expectedActions = [actions.READING_LIST_LOADING, actions.READING_LIST_LOADED];
 
-        await mockActionsStore.dispatch(loadGuides('FREN1010'));
-        expect(mockActionsStore.getActions()).toHaveDispatchedActions(expectedActions);
+            await mockActionsStore.dispatch(loadReadingLists('FREN1010', 'St Lucia', 'Semester 2 2020'));
+            expect(mockActionsStore.getActions()).toHaveDispatchedActions(expectedActions);
+        });
+
+        it('handles reading list with items of various importance', async () => {
+            mockApi
+                .onGet(
+                    repositories.routes.READING_LIST_API({
+                        coursecode: 'HIST1201',
+                        campus: 'St Lucia',
+                        semester: 'Semester 2 2020',
+                    }).apiUrl,
+                )
+                .reply(200, HIST1201ReadingList);
+
+            const expectedActions = [actions.READING_LIST_LOADING, actions.READING_LIST_LOADED];
+
+            await mockActionsStore.dispatch(loadReadingLists('HIST1201', 'St Lucia', 'Semester 2 2020'));
+            expect(mockActionsStore.getActions()).toHaveDispatchedActions(expectedActions);
+        });
+
+        it('dispatches expected actions when possible reading list call fails', async () => {
+            mockApi
+                .onGet(
+                    repositories.routes.READING_LIST_API({
+                        coursecode: 'FREN1010',
+                        campus: 'St Lucia',
+                        semester: 'Semester 2 2020',
+                    }).apiUrl,
+                )
+                .reply(500);
+
+            const expectedActions = [actions.READING_LIST_LOADING, actions.APP_ALERT_SHOW, actions.READING_LIST_FAILED];
+
+            await mockActionsStore.dispatch(loadReadingLists('FREN1010', 'St Lucia', 'Semester 2 2020'));
+            expect(mockActionsStore.getActions()).toHaveDispatchedActions(expectedActions);
+        });
     });
-
-    it('dispatches expected actions when possible exams call fails', async () => {
-        mockApi.onGet(repositories.routes.LEARNING_RESOURCES_EXAMS_API({ keyword: 'FREN1010' })).reply(500);
-
-        const expectedActions = [actions.EXAMS_LEARNING_RESOURCES_LOADING, actions.EXAMS_LEARNING_RESOURCES_FAILED];
-
-        await mockActionsStore.dispatch(loadExamLearningResources('FREN1010'));
-        expect(mockActionsStore.getActions()).toHaveDispatchedActions(expectedActions);
-    });
-
-    it('should dispatch clear exams list action', async () => {
-        const expectedActions = [actions.EXAMS_LEARNING_RESOURCES_CLEAR];
-
-        await mockActionsStore.dispatch(clearExamLearningResources());
-        expect(mockActionsStore.getActions()).toHaveDispatchedActions(expectedActions);
-    });
-
-    it('handles an exams get request', async () => {
-        mockApi.onGet(repositories.routes.LEARNING_RESOURCES_EXAMS_API({ keyword: 'FREN1010' }).apiUrl).reply(200, {});
-
-        const expectedActions = [actions.EXAMS_LEARNING_RESOURCES_LOADING, actions.EXAMS_LEARNING_RESOURCES_LOADED];
-
-        await mockActionsStore.dispatch(loadExamLearningResources('FREN1010'));
-        expect(mockActionsStore.getActions()).toHaveDispatchedActions(expectedActions);
-    });
-
-    // should never happen
-    it('handles an empty reading list', async () => {
-        const emptyReadingList = {
-            title: 'FREN1010',
-            course_title: 'Introductory French 1',
-            reading_lists: [
-                {
-                    campus: 'St Lucia',
-                    url: 'http://lr.library.uq.edu.au/lists/162A2ECA-2921-8A7C-CE27-C0B38055B9D7',
-                    period: 'Semester 2 2020',
-                    list: [],
-                },
-            ],
-        };
-        mockApi
-            .onGet(
-                repositories.routes.READING_LIST_API({
-                    coursecode: 'FREN1010',
-                    campus: 'St Lucia',
-                    semester: 'Semester 2 2020',
-                }).apiUrl,
-            )
-            .reply(200, emptyReadingList);
-
-        const expectedActions = [actions.READING_LIST_LOADING, actions.READING_LIST_LOADED];
-
-        await mockActionsStore.dispatch(loadReadingLists('FREN1010', 'St Lucia', 'Semester 2 2020'));
-        expect(mockActionsStore.getActions()).toHaveDispatchedActions(expectedActions);
-    });
-
-    it('handles reading list with items of various importance', async () => {
-        mockApi
-            .onGet(
-                repositories.routes.READING_LIST_API({
-                    coursecode: 'HIST1201',
-                    campus: 'St Lucia',
-                    semester: 'Semester 2 2020',
-                }).apiUrl,
-            )
-            .reply(200, HIST1201ReadingList);
-
-        const expectedActions = [actions.READING_LIST_LOADING, actions.READING_LIST_LOADED];
-
-        await mockActionsStore.dispatch(loadReadingLists('HIST1201', 'St Lucia', 'Semester 2 2020'));
-        expect(mockActionsStore.getActions()).toHaveDispatchedActions(expectedActions);
-    });
-
-    it('dispatches expected actions when possible reading list call fails', async () => {
-        mockApi
-            .onGet(
-                repositories.routes.READING_LIST_API({
-                    coursecode: 'FREN1010',
-                    campus: 'St Lucia',
-                    semester: 'Semester 2 2020',
-                }).apiUrl,
-            )
-            .reply(500);
-
-        const expectedActions = [actions.READING_LIST_LOADING, actions.APP_ALERT_SHOW, actions.READING_LIST_FAILED];
-
-        await mockActionsStore.dispatch(loadReadingLists('FREN1010', 'St Lucia', 'Semester 2 2020'));
-        expect(mockActionsStore.getActions()).toHaveDispatchedActions(expectedActions);
-    });
-
-    it('dispatches expected actions when possible suggestions call fails', async () => {
-        global.fetch = jest.fn(() => {
-            return Promise.resolve({
-                ok: false,
-                statusText: 'Internal Server Error',
-                status: 500,
+    describe('Learning Resource Suggestions actions', () => {
+        it('dispatches expected actions when possible suggestions call fails', async () => {
+            global.fetch = jest.fn(() => {
+                return Promise.resolve({
+                    ok: false,
+                    statusText: 'Internal Server Error',
+                    status: 500,
+                });
             });
+
+            const expectedActions = [
+                actions.LEARNING_RESOURCE_SUGGESTIONS_LOADING,
+                actions.LEARNING_RESOURCE_SUGGESTIONS_FAILED,
+            ];
+
+            await mockActionsStore.dispatch(loadCourseReadingListsSuggestions('FREN1010'));
+            expect(mockActionsStore.getActions()).toHaveDispatchedActions(expectedActions);
         });
 
-        const expectedActions = [
-            actions.LEARNING_RESOURCE_SUGGESTIONS_LOADING,
-            actions.LEARNING_RESOURCE_SUGGESTIONS_FAILED,
-        ];
+        it('dispatches expected actions when the response is invalid', async () => {
+            global.fetch = jest.fn(() => {
+                return Promise.resolve({});
+            });
 
-        await mockActionsStore.dispatch(loadCourseReadingListsSuggestions('FREN1010'));
-        expect(mockActionsStore.getActions()).toHaveDispatchedActions(expectedActions);
-    });
+            const expectedActions = [
+                actions.LEARNING_RESOURCE_SUGGESTIONS_LOADING,
+                actions.LEARNING_RESOURCE_SUGGESTIONS_FAILED,
+            ];
 
-    it('dispatches expected actions when the response is invalid', async () => {
-        global.fetch = jest.fn(() => {
-            return Promise.resolve({});
+            await mockActionsStore.dispatch(loadCourseReadingListsSuggestions('FREN1010'));
+            expect(mockActionsStore.getActions()).toHaveDispatchedActions(expectedActions);
         });
 
-        const expectedActions = [
-            actions.LEARNING_RESOURCE_SUGGESTIONS_LOADING,
-            actions.LEARNING_RESOURCE_SUGGESTIONS_FAILED,
-        ];
+        it('should dispatch clear course suggestions list action', async () => {
+            const expectedActions = [actions.LEARNING_RESOURCE_SUGGESTIONS_CLEAR];
 
-        await mockActionsStore.dispatch(loadCourseReadingListsSuggestions('FREN1010'));
-        expect(mockActionsStore.getActions()).toHaveDispatchedActions(expectedActions);
-    });
-
-    it('should dispatch clear course suggestions list action', async () => {
-        const expectedActions = [actions.LEARNING_RESOURCE_SUGGESTIONS_CLEAR];
-
-        await mockActionsStore.dispatch(clearLearningResourceSuggestions());
-        expect(mockActionsStore.getActions()).toHaveDispatchedActions(expectedActions);
+            await mockActionsStore.dispatch(clearLearningResourceSuggestions());
+            expect(mockActionsStore.getActions()).toHaveDispatchedActions(expectedActions);
+        });
     });
 });
