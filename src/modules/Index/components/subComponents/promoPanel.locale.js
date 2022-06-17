27d28ff074a4cda45ecc16c2037d2672088c55f9
uqjtilse <j.tilse@library.uq.edu.au>
2022-06-17 14:53:06 +1000

    [PT# 182495990]
    Spacing and paragraph break update.

--- src/modules/Index/components/subComponents/promoPanel.locale.js
+++ src/modules/Index/components/subComponents/promoPanel.locale.js
@@ -10,15 +10,15 @@ export const promoPanel = {
         title: 'Mid-year break',
         content: (
             <div>
-                <p>
-                    Congratulations to our new graduates! Visit our 
-                    <a href="https://web.library.uq.edu.au/library-services/services-for-uq-alumni">services for alumni</a> page to apply 
+                <p>Congratulations to our new graduates!</p>
+                <p>         
+                    Visit our <a href="https://web.library.uq.edu.au/library-services/services-for-uq-alumni">services for alumni</a> page to apply 
                     for your New Graduate Alumni Membership (free for the first year) and for resources available to alumni members.
                 </p>
                 <p>
-                    To our returning students, you have access to the library throughout the mid-semester break if you're 
-                    enrolled for Semester Two. Enjoy your break and we'll see you in July!
+                    To our returning students, you have access to the library throughout the mid-semester break if you're enrolled for Semester Two. 
                 </p>
+                <p>Enjoy your break and we'll see you in July!</p>   
             </div>
         ),
     },
@@ -26,15 +26,15 @@ export const promoPanel = {
         title: 'Mid-year break',
         content: (
             <div>
-                <p>
-                    Congratulations to our new graduates! Visit our 
-                    <a href="https://web.library.uq.edu.au/library-services/services-for-uq-alumni">services for alumni</a> page to apply 
+                <p>Congratulations to our new graduates!</p>
+                <p>         
+                    Visit our <a href="https://web.library.uq.edu.au/library-services/services-for-uq-alumni">services for alumni</a> page to apply 
                     for your New Graduate Alumni Membership (free for the first year) and for resources available to alumni members.
                 </p>
                 <p>
-                    To our returning students, you have access to the library throughout the mid-semester break if you're 
-                    enrolled for Semester Two. Enjoy your break and we'll see you in July!
+                    To our returning students, you have access to the library throughout the mid-semester break if you're enrolled for Semester Two. 
                 </p>
+                <p>Enjoy your break and we'll see you in July!</p>   
             </div>
         ),
     },
