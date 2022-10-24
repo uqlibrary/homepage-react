d31f548d0e90846c290e42c2d7bdfb3142e671aa
Lee Sibbald <lsibbald100+git@gmail.com>
2022-10-25 09:50:21 +1000

    [#183566416] test tweak due to changed anon routes

--- src/config/routes.test.js
+++ src/config/routes.test.js
@@ -4,7 +4,7 @@ describe('Routes getRoutesConfig method', () => {
     describe('Routes getRoutesConfig method', () => {
         it('should return a list of routes for anon user', () => {
             const testRoutes = routes.getRoutesConfig({ components: {}, account: null });
-            expect(testRoutes.length).toEqual(6);
+            expect(testRoutes.length).toEqual(7);
         });
     });
 });
