14416dc04095b466f0ab168ca4b94552248c0ca5
Lee Sibbald <lsibbald100+git@gmail.com>
2022-11-22 15:53:19 +1000

    [#183833825] text abstracted to locale file

--- src/modules/Pages/Admin/TestTag/testTag.locale.js
+++ src/modules/Pages/Admin/TestTag/testTag.locale.js
@@ -1,10 +1,122 @@
-// import React from 'react';
-
 export default {
+    config: {
+        dateFormat: 'YYYY-MM-DD HH:MM',
+        dateFormatNoTime: 'YYYY-MM-DD',
+        dateFormatDisplay: 'Do MMMM, YYYY',
+        currentLabel: 'CURRENT',
+        passLabel: 'PASS',
+        failedLabel: 'FAIL',
+        repairLabel: 'REPAIR',
+        discardedLabel: 'DISCARD',
+        noneLabel: 'NONE',
+    },
     form: {
+        loading: 'Loading...',
         pageTitle: 'UQ Asset Test and Tag',
-        testingPage: {
-            header: 'Managing assets for Technical Services',
+        pageSubtitle: dept => `Managing Assets for ${dept}`,
+        requiredText: 'All fields are required unless otherwise stated.',
+        event: {
+            title: 'Event',
+            aria: {
+                collapseButtonLabel: 'Show more',
+            },
+            date: {
+                label: 'Event date',
+            },
+            location: {
+                title: 'Location',
+                siteLabel: 'Site',
+                building: {
+                    label: 'Building',
+                },
+                floor: {
+                    label: 'Floor',
+                },
+                room: { label: 'Room' },
+            },
+        },
+        asset: {
+            title: 'Asset',
+            addText: pid => `Add ${pid}`,
+            assetId: {
+                label: 'Asset ID',
+                helperText: 'Enter a new ID to add',
+                placeholder: 'Enter at least 7 characters',
+            },
+            assetType: {
+                label: 'Asset type',
+            },
+            ownerLabel: 'Asset owner',
+        },
+        lastTestPanel: {
+            title: suffix => `Previous Test ${suffix}`,
+            aria: {
+                collapseButtonLabel: 'Show more',
+            },
+            statusLabel: 'Status: ',
+            statusUnavailableLabel: 'Unavailable',
+            statusUnknownLabel: 'UNKNOWN',
+            testDateLabel: 'Test Date: ',
+            siteLabel: 'Site: ',
+            buildingLabel: 'Building: ',
+            floorLabel: 'Floor: ',
+            roomLabel: 'Room: ',
+            alertLocationMismatch: 'Locations do not match',
+            failReasonLabel: 'Fail Reason: ',
+            testNotesLabel: 'Test Notes: ',
+            nextTestDateLabel: 'Next Test Date: ',
+            noneLabel: 'None',
+        },
+        inspection: {
+            title: 'Inspection',
+            deviceLabel: 'Testing device',
+            testResultLabel: 'Test Result',
+            nextTestDateLabel: 'Next test due',
+            nextTestDateFormatted: dateStr => `Next test due ${dateStr}`,
+            failReason: {
+                label: 'Fail Reason',
+            },
+            inspectionNotes: {
+                label: 'Inspection Notes',
+            },
+        },
+        action: {
+            title: 'Action',
+            tabs: [
+                {
+                    label: 'Repair',
+                    value: 1,
+                },
+                {
+                    label: 'Discard',
+                    value: 2,
+                },
+            ],
+            repair: {
+                label: 'Send for Repair?',
+                options: [
+                    { label: 'NO', value: 1 },
+                    { label: 'YES', value: 2 },
+                ],
+                repairerDetails: {
+                    label: 'Repairer Details',
+                },
+            },
+            discard: {
+                alertMessage: 'IMPORTANT: Only complete this section if you are actually discarding the asset.',
+                label: 'Discard this Asset?',
+                options: [
+                    { label: 'NO', value: 1 },
+                    { label: 'YES', value: 2 },
+                ],
+                discardReason: {
+                    label: 'Discarding Reason',
+                },
+            },
+        },
+        buttons: {
+            cancel: 'CANCEL',
+            save: 'SAVE',
         },
         saveSuccessConfirmation: {
             confirmationTitle: 'Inspection and/or action successfully saved.',
