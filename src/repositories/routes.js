8a127cee04b433335d64166fe61c07375e1e0d90
Steve Lancaster <90990468+SteveLancaster@users.noreply.github.com>
2022-12-14 09:04:21 +1000

    Update routes.js

--- src/repositories/routes.js
+++ src/repositories/routes.js
@@ -168,7 +168,7 @@ export const SPOTLIGHT_DELETE_BULK_API = () => ({ apiUrl: 'spotlight/bulk' });
 export const PROMOPANEL_CREATE_API = () => ({ apiUrl: 'promo_panel/panel' });
 export const PROMOPANEL_UPDATE_API = ({ id }) => ({ apiUrl: `promo_panel/panel/${id}` });
 export const PROMOPANEL_LIST_USERTYPES_API = () => ({ apiUrl: 'promo_panel/user_groups' });
-export const PROMOPANEL_LIST_ACTIVE_PANELS_API = () => ({ apiUrl: 'promo_panel/user_groups/active' });
+export const PROMOPANEL_LIST_ACTIVE_PANELS_API = () => ({ apiUrl: 'promo_panel/groups/active' });
 export const PROMOPANEL_LIST_API = () => ({ apiUrl: 'promo_panel/panels' });
 export const PROMOPANEL_GET_CURRENT_API = () => ({ apiUrl: 'promo_panel' });
 export const PROMOPANEL_GET_ANON_API = () => ({ apiUrl: 'promo_panel/loggedout' });
