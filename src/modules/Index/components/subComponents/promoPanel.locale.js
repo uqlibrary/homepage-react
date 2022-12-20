86d151d4bcbc5fe842afd36ba0c5646187516604
Lea de Groot <l.degroot@uq.edu.au>
2022-12-21 09:03:55 +1000

    Summer Semester content - fix html [PT #184077777]

--- src/modules/Index/components/subComponents/promoPanel.locale.js
+++ src/modules/Index/components/subComponents/promoPanel.locale.js
@@ -14,7 +14,6 @@ export const promoPanel = {
                 <p>Enjoy a range of comfortable study spaces. Access 24/7 spaces and collections with your UQ ID card. Check our <a href="https://web.library.uq.edu.au/locations-hours/opening-hours">opening hours</a> for details.</p>
                 <p><a href="https://web.library.uq.edu.au/contact-us">Contact us</a> or visit an AskUs service point for assistance.</p>
             </div>
-            </div>
         ),
     },
     loggedin: {
