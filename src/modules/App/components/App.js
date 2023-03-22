5f063243cd8d101c719043123cbd38162699da9f
Lea de Groot <l.degroot@uq.edu.au>
2023-03-22 14:05:42 +1000

    fix mis merge

--- src/modules/App/components/App.js
+++ src/modules/App/components/App.js
@@ -131,7 +131,6 @@ export const App = ({ account, actions }) => {
                     <connect-footer />
                     <uq-footer />
                     <proactive-chat />
-                    <cultural-advice-popup />
                 </div>
             </div>
         </Grid>
