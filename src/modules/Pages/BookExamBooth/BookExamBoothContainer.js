8d3db7f91be3023584e5384079a5699a67ba3518
Lea de Groot <l.degroot@uq.edu.au>
2023-05-05 11:42:39 +1000

    simplify the Web Component file structure [PT #185015885]

--- src/modules/Pages/BookExamBooth/BookExamBoothContainer.js
+++ src/modules/Pages/BookExamBooth/BookExamBoothContainer.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import moment from 'moment';
 
-import BookExamBooth from '../components/BookExamBooth';
+import BookExamBooth from './BookExamBooth';
 
 const BookExamBoothContainer = () => {
     /**
