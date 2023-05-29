e14427ae8c355022ac82ffbd442881281dc86b47
Lee Sibbald <lsibbald100+git@gmail.com>
2023-05-29 14:20:41 +1000

    Created separate pathConfig file and adjusted imports of pathConfig to be relative rather than using globals. This should prevent the cypress error with webpack 4. Also tweaked a few annoying deprecated warnings re: justify vs justifyContent

--- src/config/routes.js
+++ src/config/routes.js
@@ -6,42 +6,11 @@ import {
     isTestTagAdminUser,
     isPromoPanelAdminUser,
 } from 'helpers/access';
+import { pathConfig } from './pathConfig';
 
 export const fullPath = process.env.FULL_PATH || 'https://homepage-staging.library.uq.edu.au';
 
 export const adminEditRegexConfig = new RegExp(/\/admin\/alerts\/edit\/(.*)/i);
-
-export const pathConfig = {
-    index: '/',
-    learningresources: '/learning-resources',
-    paymentReceipt: '/payment-receipt',
-    admin: {
-        alertsadd: '/admin/alerts/add',
-        alertsedit: alertid => `/admin/alerts/edit/${alertid}`,
-        alertsclone: alertid => `/admin/alerts/clone/${alertid}`,
-        alertsview: alertid => `/admin/alerts/view/${alertid}`,
-        alerts: '/admin/alerts',
-        masquerade: '/admin/masquerade',
-        promopaneladd: '/admin/promopanel/add',
-        promopaneledit: promopanelid => `/admin/promopanel/edit/${promopanelid}`,
-        // promopanelview: promopanelid => `/admin/promopanel/view/${promopanelid}`,
-        promopanelclone: promopanelid => `/admin/promopanel/clone/${promopanelid}`,
-        promopanel: '/admin/promopanel',
-        spotlightsadd: '/admin/spotlights/add',
-        spotlightsedit: spotlightid => `/admin/spotlights/edit/${spotlightid}`,
-        spotlightsview: spotlightid => `/admin/spotlights/view/${spotlightid}`,
-        spotlightsclone: spotlightid => `/admin/spotlights/clone/${spotlightid}`,
-        spotlights: '/admin/spotlights',
-        testntagdashboard: '/admin/testntag',
-        testntaginspect: '/admin/testntag/inspect',
-        testntagmanage: '/admin/testntag/manage',
-        testntagreport: '/admin/testntag/report',
-    },
-    bookExamBooth: '/book-exam-booth',
-    pastExamPaperList: courseHint => `/exams/course/${courseHint}`,
-    pastExamPaperSearch: '/exams',
-    help: 'https://guides.library.uq.edu.au/for-researchers/research-publications-guide',
-};
 
 // a duplicate list of routes for checking validity easily, 2 sets: exact match and startswith
 export const flattedPathConfigExact = [
