eefaa53e0e20a09063b74c342db81b600856736d
Lee Sibbald <lsibbald100+git@gmail.com>
2023-07-25 17:37:25 +1000

    [#185496659] Jest tests for AuthWrapper

--- src/modules/Pages/Admin/TestTag/SharedComponents/AuthWrapper/AuthWrapper.js
+++ src/modules/Pages/Admin/TestTag/SharedComponents/AuthWrapper/AuthWrapper.js
@@ -4,11 +4,10 @@ import { useSelector } from 'react-redux';
 
 import { hasAccess } from '../../helpers/auth';
 
-const AuthWrapper = ({ requiredPermissions = [], inclusive = false, fallback = <></>, children } = {}) => {
+const AuthWrapper = ({ requiredPermissions = [], inclusive = false, fallback, children } = {}) => {
     const { privilege } = useSelector(state => state.get('testTagUserReducer'));
     const shouldHaveAccess = hasAccess(privilege, requiredPermissions, inclusive);
-
-    return shouldHaveAccess ? children : fallback ?? <></>;
+    return shouldHaveAccess ? children : fallback ?? /* istanbul ignore next */ <></>;
 };
 
 AuthWrapper.propTypes = {
