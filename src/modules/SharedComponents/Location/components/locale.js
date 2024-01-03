1ce398ed8bb72c71c5d69843dffde982d8c2b794
Lea de Groot <l.degroot@glibrary.uq.edu.au>
2024-01-04 09:30:30 +1000

    refactor [PT #186775493]

--- src/modules/SharedComponents/Location/components/locale.js
+++ src/modules/SharedComponents/Location/components/locale.js
@@ -1,3 +1,8 @@
+const CAMPUS_ST_LUCIA = 'St Lucia';
+const CAMPUS_GATTON = 'Gatton';
+const CAMPUS_HERSTON = 'Herston';
+const CAMPUS_DUTTON_PARK = 'Dutton Park';
+
 export const locale = {
     tooltip: 'Click to update your preferred campus, currently [currentLocation]',
     noLocationSet: 'not set',
@@ -5,55 +10,55 @@ export const locale = {
     menuTitle: 'Select a preferred location',
     locations: [
         {
-            location: 'No preference',
+            displayName: 'No preference',
             value: 'not set',
         },
         {
-            location: 'St Lucia',
-            value: 'St Lucia',
+            displayName: 'St Lucia',
+            value: CAMPUS_ST_LUCIA,
         },
         {
-            location: 'Gatton',
-            value: 'Gatton',
+            displayName: 'Gatton',
+            value: CAMPUS_GATTON,
         },
         {
-            location: 'Herston',
-            value: 'Herston',
+            displayName: 'Herston',
+            value: CAMPUS_HERSTON,
         },
         {
-            location: 'Dutton Park',
-            value: 'Dutton Park',
+            displayName: 'Dutton Park',
+            value: CAMPUS_DUTTON_PARK,
         },
     ],
     // tie a campus to a building, for highlighting the chosen location
     computersCampusMap: {
         // the keys in this must match the names found in the Computer Availablity api result
-        'Architecture &amp; Music Library': 'St Lucia',
-        'Biological Sciences Library': 'St Lucia',
-        'Central Library': 'St Lucia',
-        'D.H. Engineering &amp; Sciences Library': 'St Lucia',
-        'Duhig Building': 'St Lucia',
-        'Fryer Library': 'St Lucia',
-        'Gatton Campus Library': 'Gatton',
-        'Herston Health Sciences Library': 'Herston',
-        'Law Library': 'St Lucia',
+        'Architecture &amp; Music Library': CAMPUS_ST_LUCIA,
+        'Biological Sciences Library': CAMPUS_ST_LUCIA,
+        'Central Library': CAMPUS_ST_LUCIA,
+        'D.H. Engineering &amp; Sciences Library': CAMPUS_ST_LUCIA,
+        'Duhig Building': CAMPUS_ST_LUCIA,
+        'Fryer Library': CAMPUS_ST_LUCIA,
+        'Gatton Campus Library': CAMPUS_GATTON,
+        'Herston Health Sciences Library': CAMPUS_HERSTON,
+        'Law Library': CAMPUS_ST_LUCIA,
         'Whitty Mater': 'Other',
-        'Dutton Park Health Sciences Library': 'Dutton Park',
+        'Dutton Park Health Sciences Library': CAMPUS_DUTTON_PARK,
     },
     hoursCampusMap: {
         // the keys in this must match the abbr values found in the Hours api result
-        'Arch Music': 'St Lucia',
+        'Arch Music': CAMPUS_ST_LUCIA,
         AskUs: 'Online',
-        'Biol Sci': 'St Lucia',
-        Central: 'St Lucia',
-        DHEngSci: 'St Lucia',
-        'Duhig Study': 'St Lucia',
-        Fryer: 'St Lucia',
-        Gatton: 'Gatton',
-        Herston: 'Herston',
-        Law: 'St Lucia',
+        'Biol Sci': CAMPUS_ST_LUCIA,
+        Central: CAMPUS_ST_LUCIA,
+        DHEngSci: CAMPUS_ST_LUCIA,
+        'Duhig Study': CAMPUS_ST_LUCIA,
+        Fryer: CAMPUS_ST_LUCIA,
+        Gatton: CAMPUS_GATTON,
+        Herston: CAMPUS_HERSTON,
+        Law: CAMPUS_ST_LUCIA,
         'Whitty Mater': 'Other',
-        'Dutton Park': 'Dutton Park',
+        'Dutton Park': CAMPUS_DUTTON_PARK,
         Bundaberg: 'Other',
         HerveyBay: 'Other',
         Rockhampton: 'Other',
