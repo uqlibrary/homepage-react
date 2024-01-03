001c736a02aa8ec27250eb4be0e8ef87e6465b7c
Lea de Groot <l.degroot@glibrary.uq.edu.au>
2024-01-04 09:17:02 +1000

    refactor: move the campus maps to the actual dependency, Locations [PT #186677955]

--- src/modules/SharedComponents/Location/components/locale.js
+++ src/modules/SharedComponents/Location/components/locale.js
@@ -25,4 +25,38 @@ export const locale = {
             value: 'Dutton Park',
         },
     ],
+    // tie a campus to a building, for highlighting the chosen location
+    computersCampusMap: {
+        // the keys in this must match the names found in the Computer Availablity api result
+        'Architecture &amp; Music Library': 'St Lucia',
+        'Biological Sciences Library': 'St Lucia',
+        'Central Library': 'St Lucia',
+        'D.H. Engineering &amp; Sciences Library': 'St Lucia',
+        'Duhig Building': 'St Lucia',
+        'Fryer Library': 'St Lucia',
+        'Gatton Campus Library': 'Gatton',
+        'Herston Health Sciences Library': 'Herston',
+        'Law Library': 'St Lucia',
+        'Whitty Mater': 'Other',
+        'Dutton Park Health Sciences Library': 'Dutton Park',
+    },
+    hoursCampusMap: {
+        // the keys in this must match the abbr values found in the Hours api result
+        'Arch Music': 'St Lucia',
+        AskUs: 'Online',
+        'Biol Sci': 'St Lucia',
+        Central: 'St Lucia',
+        DHEngSci: 'St Lucia',
+        'Duhig Study': 'St Lucia',
+        Fryer: 'St Lucia',
+        Gatton: 'Gatton',
+        Herston: 'Herston',
+        Law: 'St Lucia',
+        'Whitty Mater': 'Other',
+        'Dutton Park': 'Dutton Park',
+        Bundaberg: 'Other',
+        HerveyBay: 'Other',
+        Rockhampton: 'Other',
+        Toowoomba: 'Other',
+    },
 };
