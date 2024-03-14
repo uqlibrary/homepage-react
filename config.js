6a9ab870604441ff5145826a5cbb533125587ada
Lea de Groot <l.degroot@glibrary.uq.edu.au>
2024-03-14 11:57:30 +1000

    call dev branch reusable to see navigation panels

--- config.js
+++ config.js
@@ -14,8 +14,9 @@ const deployment = {
         basePath: '',
         publicPath: '',
         // reusablejs: 'http://localhost:8080/uq-lib-reusable.min.js',
-        // reusablejs: 'https://assets.library.uq.edu.au/reusable-webcomponents-development/[BRANCH_NAME]/uq-lib-reusable.min.js', // dev
-        reusablejs: 'https://assets.library.uq.edu.au/reusable-webcomponents/uq-lib-reusable.min.js',
+        reusablejs:
+            'https://assets.library.uq.edu.au/reusable-webcomponents-development/redo-nav/uq-lib-reusable.min.js', // dev
+        // reusablejs: 'https://assets.library.uq.edu.au/reusable-webcomponents/uq-lib-reusable.min.js',
         removeConsoleLog: false,
     },
     staging: {
