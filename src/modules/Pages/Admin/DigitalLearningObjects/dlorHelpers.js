52a9836e49cb40cf91bd2b9ca91207b1eddf71de
Lea de Groot <l.degroot@glibrary.uq.edu.au>
2024-04-04 12:23:02 +1000

    splitting keywords, cleanup :( [PT #187101388]

--- src/modules/Pages/Admin/DigitalLearningObjects/dlorHelpers.js
+++ src/modules/Pages/Admin/DigitalLearningObjects/dlorHelpers.js
@@ -4,12 +4,17 @@ export function splitStringToArrayOnComma(keywordString) {
         // split 'abc, "def, def", "hij"'
         // to ['abc', 'def, def', 'hij']
         splitStringToArrayOnComma = keywordString
+            .replace(/[^a-zA-Z0-9- ,"]/g, '')
             .replace(/,/g, ', ') // if they didnt put a space after the comma, add one
             .replace(/,  /g, ', ') // (then correct any doubles)
             .trim()
-            .split(/, (?=(?:(?:[^"]*"){2})*[^"]*$)/);
+            .split(/, (?=(?:(?:[^"]*"){2})*[^"]*$)/); // split on the comma, except commas inside quotes
         if (!!splitStringToArrayOnComma && splitStringToArrayOnComma.length > 0) {
-            splitStringToArrayOnComma = splitStringToArrayOnComma.map(keyword => keyword.replace(/^"|"$/g, '').trim());
+            splitStringToArrayOnComma = splitStringToArrayOnComma.map(keyword => {
+                return keyword
+                    .replace(/^"|"$/g, '') // get rid of surrounding quotes
+                    .trim();
+            });
         }
     }
     return splitStringToArrayOnComma;
--- src/modules/Pages/Admin/DigitalLearningObjects/dlorHelpers.test.js
+++ src/modules/Pages/Admin/DigitalLearningObjects/dlorHelpers.test.js
@@ -5,6 +5,7 @@ describe('helpers', () => {
         expect(splitStringToArrayOnComma('abc, def, hij')).toEqual(['abc', 'def', 'hij']);
         expect(splitStringToArrayOnComma('abc, d"e"f, hij')).toEqual(['abc', 'd"e"f', 'hij']);
         expect(splitStringToArrayOnComma('abc, "def, def", "hij"')).toEqual(['abc', 'def, def', 'hij']);
+        expect(splitStringToArrayOnComma('"abc, abc", def, "hij"')).toEqual(['abc, abc', 'def', 'hij']);
         expect(splitStringToArrayOnComma('abc')).toEqual(['abc']);
         expect(splitStringToArrayOnComma('abc,def,hij')).toEqual(['abc', 'def', 'hij']);
     });
