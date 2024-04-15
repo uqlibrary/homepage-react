0a55086e251c87971f7957b7d9be051ab25962e4
Lea de Groot <l.degroot@glibrary.uq.edu.au>
2024-04-15 13:58:50 +1000

    refactor [PT #187101372]

--- src/modules/App/components/pages.js
+++ src/modules/App/components/pages.js
@@ -21,8 +21,8 @@ export const SpotlightsClone = lazy(() => lazyRetry(() => import('modules/Pages/
 export const DLOList = lazy(() => lazyRetry(() => import('modules/Pages/DigitalLearningObjects/Index/DLOListContainer')));
 export const DLOView = lazy(() => lazyRetry(() => import('modules/Pages/DigitalLearningObjects/View/DLOViewContainer')));
 export const DLOAdminHomepage = lazy(() => lazyRetry(() => import('modules/Pages/Admin/DigitalLearningObjects/Homepage/DLOAdminHomepageContainer')));
-export const DLOAdd = lazy(() => lazyRetry(() => import('modules/Pages/Admin/DigitalLearningObjects/Add/DLOAddContainer')));
-export const DLOEdit = lazy(() => lazyRetry(() => import('modules/Pages/Admin/DigitalLearningObjects/Edit/DLOEditContainer')));
+export const DLOAdd = lazy(() => lazyRetry(() => import('modules/Pages/Admin/DigitalLearningObjects/Form/Add/DLOAddContainer')));
+export const DLOEdit = lazy(() => lazyRetry(() => import('modules/Pages/Admin/DigitalLearningObjects/Form/Edit/DLOEditContainer')));
 
 export const TestTagDashboard = lazy(() => lazyRetry(() => import('modules/Pages/Admin/TestTag/Dashboard/containers/Dashboard')));
 export const TestTagInspection = lazy(() => lazyRetry(() => import('modules/Pages/Admin/TestTag/Inspection/containers/Inspection')));
