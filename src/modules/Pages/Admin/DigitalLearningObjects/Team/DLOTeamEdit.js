ad560185495a2f81ac3e8a1426825ad9a07f767d
Lea de Groot <l.degroot@library.uq.edu.au>
2024-05-20 13:34:07 +1000

    abstract teams edit out to generic form [PT #187444638]

--- src/modules/Pages/Admin/DigitalLearningObjects/Team/DLOTeamEdit.js
+++ src/modules/Pages/Admin/DigitalLearningObjects/Team/DLOTeamEdit.js
@@ -1,24 +1,16 @@
 import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import { useParams } from 'react-router';
-import { useCookies } from 'react-cookie';
 
-import Button from '@mui/material/Button';
-import FormControl from '@mui/material/FormControl';
 import Grid from '@mui/material/Grid';
-import Input from '@mui/material/Input';
-import InputLabel from '@mui/material/InputLabel';
 import { makeStyles } from '@mui/styles';
 import Typography from '@mui/material/Typography';
 import ArrowForwardIcon from '@mui/icons-material/ArrowForwardIos';
 
+import DlOTeamForm from 'modules/Pages/Admin/DigitalLearningObjects/Team/DlOTeamForm';
+
 import { StandardPage } from 'modules/SharedComponents/Toolbox/StandardPage';
-import { InlineLoader } from 'modules/SharedComponents/Toolbox/Loaders';
-import { dlorAdminLink, isValidEmail } from 'modules/Pages/Admin/DigitalLearningObjects/dlorAdminHelpers';
-import { fullPath } from 'config/routes';
-import { useConfirmationState } from 'hooks';
-import { ConfirmationBox } from 'modules/SharedComponents/Toolbox/ConfirmDialogBox';
-import { scrollToTopOfPage } from 'helpers/general';
+import { dlorAdminLink } from 'modules/Pages/Admin/DigitalLearningObjects/dlorAdminHelpers';
 
 const useStyles = makeStyles(theme => ({
     titleBlock: {
@@ -51,7 +43,6 @@ export const DLOTeamEdit = ({
 }) => {
     const { dlorTeamId } = useParams();
     const classes = useStyles();
-    const [cookies, setCookie] = useCookies();
     console.log('DLOTeamEdit', dlorTeamId, ' l=', dlorTeamLoading, '; e=', dlorTeamError, '; d=', dlorTeam);
     console.log(
         'DLOTeamEdit dlorUpdatedItem l=',
@@ -62,10 +53,13 @@ export const DLOTeamEdit = ({
         dlorUpdatedItem,
     );
 
-    const [formValues, setFormValues] = useState(null);
-    const [saveStatus, setSaveStatus] = useState(null); // control confirmation box display
-
-    const [isOpen, showConfirmation, hideConfirmation] = useConfirmationState();
+    // const formDefaults = {
+    //     team_id: dlorTeam.team_id,
+    //     team_name: dlorTeam.team_name,
+    //     team_email: dlorTeam.team_email,
+    //     team_manager: dlorTeam.team_manager,
+    //     objects_count: dlorTeam.objects_count,
+    // };
 
     useEffect(() => {
         if (!!dlorTeamId) {
@@ -73,81 +67,6 @@ export const DLOTeamEdit = ({
         }
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [dlorTeamId]);
-
-    useEffect(() => {
-        if (!!dlorTeam && !dlorTeamLoading && !dlorTeamError) {
-            setFormValues({
-                team_name: dlorTeam.team_name,
-                team_manager: dlorTeam.team_manager,
-                team_email: dlorTeam.team_email,
-            });
-        }
-    }, [dlorTeam, dlorTeamLoading, dlorTeamError]);
-
-    useEffect(() => {
-        if (!!dlorUpdatedItem?.data?.team_id || !!dlorUpdatedItemError) {
-            setSaveStatus('complete');
-            showConfirmation();
-        }
-        console.log('useEffect: dlorUpdatedItem dlorUpdatedItem=', dlorUpdatedItem);
-    }, [showConfirmation, dlorUpdatedItem, dlorUpdatedItemError]);
-
-    function closeConfirmationBox() {
-        setSaveStatus(null);
-        hideConfirmation();
-    }
-
-    const navigateToTeamManagementHomePage = () => {
-        // TODO also want to clear form here too before nav, so back button gives clear form?
-
-        closeConfirmationBox();
-        window.location.href = dlorAdminLink('/team/manage');
-        scrollToTopOfPage();
-    };
-
-    const clearForm = actiontype => {
-        closeConfirmationBox();
-        window.location.reload(false);
-    };
-
-    const locale = {
-        successMessage: {
-            confirmationTitle: 'Changes have been saved', // mode === 'add' ? 'The team has been created' : 'Changes have been saved',
-            confirmationMessage: '',
-            cancelButtonLabel: 'Re-edit Team', // mode === 'add' ? 'Add another Team' : 'Re-edit Team',
-            confirmButtonLabel: 'Return to Admin Teams page',
-        },
-        errorMessage: {
-            confirmationTitle: dlorUpdatedItemError,
-            confirmationMessage: '',
-            cancelButtonLabel: 'Re-edit Team', // mode === 'add' ? 'Add another Team' : 'Re-edit Team',
-            confirmButtonLabel: 'Return to Admin Teams page',
-        },
-    };
-
-    const handleChange = (prop, value) => e => {
-        console.log('handleChange', prop, e.target);
-        const theNewValue = e.target.value;
-        const newValues = { ...formValues, [prop]: theNewValue };
-        setFormValues(newValues);
-    };
-
-    const saveChanges = () => {
-        console.log('saveChanges', dlorTeamId, formValues);
-        const cypressTestCookie = cookies.hasOwnProperty('CYPRESS_TEST_DATA') ? cookies.CYPRESS_TEST_DATA : null;
-        if (!!cypressTestCookie && location.host === 'localhost:2020' && cypressTestCookie === 'active') {
-            setCookie('CYPRESS_DATA_SAVED', formValues);
-        }
-
-        actions.updateDlorTeam(dlorTeamId, formValues);
-    };
-
-    const isValidTeamName = teamName => {
-        if (teamName.trim() === '') {
-            return false;
-        }
-        return true;
-    };
 
     return (
         <StandardPage title="Digital learning hub - Edit Team">
@@ -168,120 +87,16 @@ export const DLOTeamEdit = ({
                     </div>
                 </Grid>
             </Grid>
-            <Grid container spacing={2}>
-                {(() => {
-                    if (!!dlorTeamLoading || !!dlorItemUpdating || (!dlorTeamError && !dlorTeam)) {
-                        return (
-                            <Grid item xs={12} md={9} style={{ marginTop: 12 }}>
-                                <div style={{ minHeight: 600 }}>
-                                    <InlineLoader message="Loading" />
-                                </div>
-                            </Grid>
-                        );
-                    } else if (!!dlorTeamError) {
-                        return (
-                            <Grid item xs={12} md={9} style={{ marginTop: 12 }}>
-                                <Typography variant="body1" data-testid="dlor-teamItem-error">
-                                    {dlorTeamError}
-                                </Typography>
-                            </Grid>
-                        );
-                    } else if (!!dlorTeam) {
-                        return (
-                            <>
-                                <Grid item xs={12} data-testid="dlor-team-item-list">
-                                    <Grid container key={`list-team-${dlorTeam?.team_id}`}>
-                                        {saveStatus === 'complete' && (
-                                            <ConfirmationBox
-                                                actionButtonColor="primary"
-                                                actionButtonVariant="contained"
-                                                confirmationBoxId="dlor-team-save-outcome"
-                                                onAction={() => navigateToTeamManagementHomePage()}
-                                                hideCancelButton={!locale.successMessage.cancelButtonLabel}
-                                                cancelButtonLabel={locale.successMessage.cancelButtonLabel}
-                                                onCancelAction={() => clearForm()}
-                                                onClose={closeConfirmationBox}
-                                                isOpen={isOpen}
-                                                locale={
-                                                    !!dlorUpdatedItemError ? locale.errorMessage : locale.successMessage
-                                                }
-                                            />
-                                        )}
-                                        <form id="dlor-editTeam-form" style={{ width: '100%' }}>
-                                            <Grid item xs={12}>
-                                                <FormControl variant="standard" fullWidth>
-                                                    <InputLabel htmlFor="team_name">Team name *</InputLabel>
-                                                    <Input
-                                                        id="team_name"
-                                                        data-testid="team_name"
-                                                        required
-                                                        value={formValues?.team_name || ''}
-                                                        onChange={handleChange('team_name')}
-                                                        error={!isValidTeamName(formValues?.team_name)}
-                                                    />
-                                                </FormControl>
-                                                {!isValidTeamName(formValues?.team_name) && (
-                                                    <div
-                                                        className={classes.errorMessage}
-                                                        data-testid="error-message-team_name"
-                                                    >
-                                                        This team name is not valid.
-                                                    </div>
-                                                )}
-                                            </Grid>
-                                            <Grid item xs={12}>
-                                                <FormControl variant="standard" fullWidth>
-                                                    <InputLabel htmlFor="team_manager">Name of Team Manager</InputLabel>
-                                                    <Input
-                                                        id="team_manager"
-                                                        data-testid="team_manager"
-                                                        value={formValues?.team_manager || ''}
-                                                        onChange={handleChange('team_manager')}
-                                                    />
-                                                </FormControl>
-                                            </Grid>
-                                            <Grid item xs={12}>
-                                                <FormControl variant="standard" fullWidth>
-                                                    <InputLabel htmlFor="team_email">
-                                                        Email address to contact team
-                                                    </InputLabel>
-                                                    <Input
-                                                        id="team_email"
-                                                        data-testid="team_email"
-                                                        value={formValues?.team_email || ''}
-                                                        onChange={handleChange('team_email')}
-                                                        error={!isValidEmail(formValues?.team_email)}
-                                                    />
-                                                    {!isValidEmail(formValues?.team_email) && (
-                                                        <div
-                                                            className={classes.errorMessage}
-                                                            data-testid="error-message-team_email"
-                                                        >
-                                                            This email address is not valid.
-                                                        </div>
-                                                    )}
-                                                </FormControl>
-                                            </Grid>
-                                        </form>
-                                    </Grid>
-                                </Grid>
-
-                                <Grid item xs={12} align="right">
-                                    <Button
-                                        color="primary"
-                                        data-testid="admin-dlor-teamedit-save-button"
-                                        variant="contained"
-                                        children="Save"
-                                        // disabled={!isFormValid}
-                                        onClick={saveChanges}
-                                        // className={classes.saveButton}
-                                    />
-                                </Grid>
-                            </>
-                        );
-                    }
-                })()}
-            </Grid>
+            <DlOTeamForm
+                actions={actions}
+                dlorTeam={dlorTeam}
+                dlorTeamLoading={dlorTeamLoading}
+                dlorTeamError={dlorTeamError}
+                dlorItemUpdating={dlorItemUpdating}
+                dlorUpdatedItemError={dlorUpdatedItemError}
+                dlorUpdatedItem={dlorUpdatedItem}
+                mode="edit"
+            />
         </StandardPage>
     );
 };
