54193d900a048657f2831110b9d1fdfe7ff56347
Lea de Groot <l.degroot@library.uq.edu.au>
2024-05-29 09:12:33 +1000

    tested dlor series [PT #187429426]

--- cypress/e2e/adminPages/dlor/dlorseries/dlorSeriesEdit.spec.js
+++ cypress/e2e/adminPages/dlor/dlorseries/dlorSeriesEdit.spec.js
@@ -10,6 +10,27 @@ describe('Digital Learning Hub admin Series management - edit item', () => {
             cy.visit(`http://localhost:2020/admin/dlor/series/edit/1?user=${DLOR_ADMIN_USER}`);
             cy.viewport(1300, 1000);
         });
+        it('loads as expected', () => {
+            // series name shows correctly
+            cy.get('[data-testid="object_series_name"] input')
+                .should('exist')
+                .should('have.value', 'Advanced literature searching');
+            // linked
+            cy.get('[data-testid="object_series_order-98s0_dy5k3_98h4"] input')
+                .should('exist')
+                .should('have.value', 1);
+            cy.get('[data-testid="object_series_order-9bc1894a-8b0d-46da-a25e-02d26e2e056c"] input')
+                .should('exist')
+                .should('have.value', 2);
+            // unaffiliated
+            cy.get('[data-testid="object_series_order-9bc192a8-324c-4f6b-ac50-01"] input')
+                .should('exist')
+                .should('have.value', '');
+            cy.get('[data-testid="object_series_order-9bc192a8-324c-4f6b-ac50-02"] input')
+                .should('exist')
+                .should('have.value', '');
+            // there are more, but thats probably enough
+        });
         it('is accessible', () => {
             cy.injectAxe();
             cy.viewport(1300, 1000);
@@ -22,17 +43,15 @@ describe('Digital Learning Hub admin Series management - edit item', () => {
                 includedImpacts: ['minor', 'moderate', 'serious', 'critical'],
             });
         });
-        it('loads as expected', () => {
-            // series name shows correctly
-            cy.get('[data-testid="object_series_name"] input')
-                .should('exist')
-                .should('have.value', 'Advanced literature searching');
-        });
         it('has a working "view a dlor" button', () => {
-            // can open a list and click edit
-            cy.get('[data-testid="dlor-series-edit-view-2"]')
-                .should('exist')
+            cy.get('a[target="_blank"][data-testid="dlor-series-edit-view-2"]')
+                .should('be.visible')
+                .then($a => {
+                    // Change the target attribute to _self for testing
+                    $a.attr('target', '_self');
+                })
                 .click();
+
             cy.url().should('eq', 'http://localhost:2020/digital-learning-hub/view/98s0_dy5k3_98h4');
         });
     });
