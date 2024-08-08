dcff76a21d0ed0d1bc85b4b1c334a888f2b5745e
Lea de Groot <l.degroot@library.uq.edu.au>
2024-08-08 11:46:41 +1000

    tweak supplied source a little and add to webpack [PT #188081922]

--- webpack-dist.config.js
+++ webpack-dist.config.js
@@ -121,6 +121,11 @@ const webpackConfig = {
             inject: true,
             template: resolve(__dirname, './public', 'index.html'),
         }),
+        new HtmlWebpackPlugin({
+            template: './public/chatbot.html',
+            filename: 'chatbot.html',
+            chunks: [],
+        }),
         new CopyPlugin({
             patterns: [
                 {
