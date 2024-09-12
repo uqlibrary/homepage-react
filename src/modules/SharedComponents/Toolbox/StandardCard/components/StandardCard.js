305f139ddbb870c134f0a9c64b33d5955abc5e36
Lea de Groot <l.degroot@library.uq.edu.au>
2024-09-12 15:54:48 +1000

    swap the colours for standard cards [PT #187866162]

--- src/modules/SharedComponents/Toolbox/StandardCard/components/StandardCard.js
+++ src/modules/SharedComponents/Toolbox/StandardCard/components/StandardCard.js
@@ -19,11 +19,11 @@ const StyledCard = styled(Card)(({ theme }) => ({
         paddingRight: '0px !important',
     },
     '& .cardHeaderPrimary': {
-        color: theme.palette.white.main,
+        color: theme.palette.primary.light,
         overflow: 'hidden',
         whiteSpace: 'nowrap',
         textOverflow: 'ellipsis',
-        backgroundColor: theme.palette.primary.light,
+        backgroundColor: theme.palette.white.main,
         borderRadius: '4px 4px 0px 0px',
         padding: '12px 24px',
     },
