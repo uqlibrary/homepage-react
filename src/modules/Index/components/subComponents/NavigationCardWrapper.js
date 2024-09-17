247d9671fb5c5f40e6143b3a7de13d5560767eb8
Lea de Groot <l.degroot@library.uq.edu.au>
2024-09-18 08:13:51 +1000

    reorder the panes [PT #187859137]

--- src/modules/Index/components/subComponents/NavigationCardWrapper.js
+++ src/modules/Index/components/subComponents/NavigationCardWrapper.js
@@ -24,22 +24,22 @@ const NavigationCardWrapper = () => {
             <StyledNav>
                 <StyledGridContainer container component="ul" spacing={4} data-testid="help-navigation-panel">
                     <SingleLinkCard
-                        cardLabel="Find and borrow"
-                        landingUrl="https://web.library.uq.edu.au/find-and-borrow"
-                    />
-                    <SingleLinkCard
                         cardLabel="Study and learning support"
                         landingUrl="https://web.library.uq.edu.au/study-and-learning-support"
-                    />
-                    <SingleLinkCard cardLabel="Visit" landingUrl="https://web.library.uq.edu.au/visit" />
-                    <SingleLinkCard
-                        cardLabel="Research and publish"
-                        landingUrl="https://web.library.uq.edu.au/research-and-publish"
                     />
                     <SingleLinkCard
                         cardLabel="AskUs and student IT support"
                         landingUrl="https://web.library.uq.edu.au/askus-and-it-support"
                     />
+                    <SingleLinkCard
+                        cardLabel="Research and publish"
+                        landingUrl="https://web.library.uq.edu.au/research-and-publish"
+                    />
+                    <SingleLinkCard
+                        cardLabel="Find and borrow"
+                        landingUrl="https://web.library.uq.edu.au/find-and-borrow"
+                    />
+                    <SingleLinkCard cardLabel="Visit" landingUrl="https://web.library.uq.edu.au/visit" />
                     <SingleLinkCard cardLabel="About" landingUrl="https://web.library.uq.edu.au/about-us" />
                 </StyledGridContainer>
             </StyledNav>
