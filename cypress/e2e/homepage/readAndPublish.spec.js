d918d04378f944b8a21d2aa1da979db50cb02caf
Lea de Groot <l.degroot@library.uq.edu.au>
2024-10-15 14:31:43 +1000

    make the test more debuggable [PT #188248943]

--- cypress/e2e/homepage/readAndPublish.spec.js
+++ cypress/e2e/homepage/readAndPublish.spec.js
@@ -1,6 +1,6 @@
 describe('Read and Publish (homepage)', () => {
     context('Containing elements', () => {
-        it('Shows the correct amount of items depending on the user', () => {
+        it('Shows all the favourites when the user only has a few favourites', () => {
             cy.visit('http://localhost:2020/?user=uqstaff');
             cy.viewport(1280, 900);
 
@@ -10,8 +10,10 @@ describe('Read and Publish (homepage)', () => {
                 .children()
                 .should('have.length', 2);
             cy.get('[data-testid="rp-yourfavourite-count"]').should('contain', 2);
-
+        });
+        it('Shows only 5 favourites when the user has many favourites', () => {
             cy.visit('http://localhost:2020/?user=uqresearcher');
+
             cy.viewport(1280, 900);
 
             // User with 8 items, only showing 5 in the list
@@ -21,7 +23,8 @@ describe('Read and Publish (homepage)', () => {
                 .should('have.length', 5);
             // 8 in total, but only shows 5.
             cy.get('[data-testid="rp-yourfavourite-count"]').should('contain', 8);
-
+        });
+        it('Shows an error when the api did not return', () => {
             cy.visit('http://localhost:2020/?user=uqpf');
             cy.viewport(1280, 900);
 
@@ -34,7 +37,8 @@ describe('Read and Publish (homepage)', () => {
 
             // no count should be showing
             cy.get('[data-testid="rp-yourfavourite-count"]').should('not.exist');
-
+        });
+        it('indicates when the user has not favourited any journals', () => {
             cy.visit('http://localhost:2020/?user=s2222222');
             cy.viewport(1280, 900);
 
@@ -49,8 +53,8 @@ describe('Read and Publish (homepage)', () => {
             cy.get('[data-testid="rp-yourfavourite-count"]')
                 .should('exist')
                 .should('contain', 'no favourite items');
-
-            // public access - no read and publish should show
+        });
+        it('does not show read and publish to the non logged in user', () => {
             cy.visit('http://localhost:2020/?user=public');
             cy.get('[data-testid="readpublish-panel-content"]').should('not.exist');
         });
