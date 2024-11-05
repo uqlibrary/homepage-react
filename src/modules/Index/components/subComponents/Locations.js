614f9aca17155f15486c9c0a125346ecf0307103
Lea de Groot <l.degroot@library.uq.edu.au>
2024-11-05 15:15:43 +1000

    adjust maximum busyness bar display [PT #187866162]

--- src/modules/Index/components/subComponents/Locations.js
+++ src/modules/Index/components/subComponents/Locations.js
@@ -351,6 +351,7 @@ const Locations = ({ libHours, libHoursLoading, libHoursError, vemcount, vemcoun
                         return null;
                     }
                     const minimumDisplayedPercentage = 5;
+                    const maxmumDisplayedPercentage = 100;
 
                     const vemcountBusynessPercent = vemcountPercentByLocation(springshareLocationId);
                     let calculatedBusyness;
@@ -361,6 +362,8 @@ const Locations = ({ libHours, libHoursLoading, libHoursError, vemcount, vemcoun
                     } else if (vemcountBusynessPercent < minimumDisplayedPercentage) {
                         // don't let the bar go below what shows as a small curve on the left
                         calculatedBusyness = minimumDisplayedPercentage;
+                    } else if (vemcountBusynessPercent > maxmumDisplayedPercentage) {
+                        calculatedBusyness = maxmumDisplayedPercentage;
                     } else {
                         calculatedBusyness = Math.floor(vemcountBusynessPercent);
                     }
@@ -466,7 +469,7 @@ const Locations = ({ libHours, libHoursLoading, libHoursError, vemcount, vemcoun
             <div className="occupancy">
                 <div
                     className={`occupancyPercent ${
-                        location.busyness > 96 ? 'occupancyPercentLong' : ''
+                        location.busyness > 94 ? 'occupancyPercentLong' : ''
                     } occupancyPercent${location.busyness}`}
                     style={{
                         width:
