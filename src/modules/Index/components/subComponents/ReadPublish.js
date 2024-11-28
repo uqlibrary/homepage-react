6b6614a05baabea40fe957c920568381d3c9b79d
Lea de Groot <l.degroot@library.uq.edu.au>
2024-11-28 11:41:08 +1000

    set body copy to 400 weight [PT #188248896]

--- src/modules/Index/components/subComponents/ReadPublish.js
+++ src/modules/Index/components/subComponents/ReadPublish.js
@@ -3,7 +3,14 @@ import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { linkToDrupal } from 'helpers/general';
 
+import { styled } from '@mui/material/styles';
+
 import { StandardCard } from 'modules/SharedComponents/Toolbox/StandardCard';
+
+const StyledBodyCopyDiv = styled('div')(() => ({
+    fontWeight: 400,
+    marginBlock: '16px',
+}));
 
 export const ReadPublish = () => {
     return (
@@ -16,18 +23,18 @@ export const ReadPublish = () => {
             title="Read and publish"
         >
             <div className="readpublish-panel-item" style={{ margin: '0 24px 0' }}>
-                <p>
+                <StyledBodyCopyDiv>
                     <a href="https://espace.library.uq.edu.au/journals/search/">Publish in the right journal</a>
                     <br />
                     <span>Find and evaluate the best publishing options using Journal Search.</span>
-                </p>
-                <p>
+                </StyledBodyCopyDiv>
+                <StyledBodyCopyDiv>
                     Visit{' '}
                     <Link to={linkToDrupal('/research-and-publish/open-research/read-and-publish-agreements')}>
                         Read and Publish Agreements
                     </Link>{' '}
                     for more information.
-                </p>
+                </StyledBodyCopyDiv>
             </div>
         </StandardCard>
     );
