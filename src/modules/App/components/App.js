016b9d9c578ed83994cca60fe8b1a7bad355792d
Lea de Groot <l.degroot@library.uq.edu.au>
2024-12-09 13:19:34 +1000

    don't show cultural advice to admins [PT #187866120]

--- src/modules/App/components/App.js
+++ src/modules/App/components/App.js
@@ -75,7 +75,7 @@ export const App = ({ account, actions }) => {
                 <div role="region" aria-label="UQ Library Alerts">
                     <alert-list system="homepage" />
                 </div>
-                <cultural-advice />
+                {!hideForAdmin() && <cultural-advice />}
                 <div style={{ flexGrow: 1 }}>
                     <a name="content" />
                     <AccountContext.Provider
