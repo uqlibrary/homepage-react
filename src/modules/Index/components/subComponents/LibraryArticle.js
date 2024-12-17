417589c01a181ba7765ab15b7e8a0d391cf7391e
Lea de Groot <l.degroot@library.uq.edu.au>
2024-12-17 11:39:56 +1000

    NOT spacing around the first Library update article [PT #187864378]

--- src/modules/Index/components/subComponents/LibraryArticle.js
+++ src/modules/Index/components/subComponents/LibraryArticle.js
@@ -131,7 +131,7 @@ const RenderImage = (articleindex, article, theme, isSm) => {
                     width: '100%',
                     position: 'relative',
                     paddingBottom: isSm && articleindex !== 0 ? '91.534%' : '66.667%',
-                    marginBottom: '32px',
+                    marginBottom: articleindex === 0 ? null : '32px',
                 }}
             >
                 <img
