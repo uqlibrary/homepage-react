2656df926ad3ff39636f45a34ae1187599979f1a
Lea de Groot <l.degroot@library.uq.edu.au>
2025-01-22 09:52:36 +1000

    revert to working [PT #187883113]

--- bin/codebuild-parallel.sh
+++ bin/codebuild-parallel.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # This script splits the list of Cypress test spec files into two groups,
 # with roughly equal numbers of files in each group, and writes the file
-# paths for each group to separate text files (bin/groupn.txt).
+# paths for each group to separate text files (bin/group1.txt and bin/group2.txt).
 # The script assumes that the test spec files are located in the
 # cypress/e2e directory and its subdirectories.
 
@@ -14,22 +14,26 @@ printf "\n"
 
 > bin/group1.txt
 > bin/group2.txt
-> bin/group3.txt
 echo "start \n"
 index=0
 # split the file list so an even run time is likely
-# lots in pipelines 1 & 2 and then a small number to run after the unit tests in pipeline 3
 echo "$spec_files" | awk '{
-    if (NR % 5 == 1) {
-        print > "bin/group1.txt"
-    } else if (NR % 5 == 2) {
-        print > "bin/group2.txt"
-    } else if (NR % 5 == 3) {
-        print > "bin/group3.txt"
-    } else if (NR % 5 == 4) {
+    if (NR % 2 == 1) {
         print > "bin/group1.txt"
     } else {
         print > "bin/group2.txt"
     }
 }'
-printf "split done \n"
+echo "split done\n"
+
+
+# for later:
+# # most files in pipeline 1 & 2, some in pipeline 3 after the unit tests have run
+#    mod = NR % 37
+#    if (mod < 15) {
+#        print > "bin/group1.txt"
+#    } else if (mod < 30) {
+#        print > "bin/group2.txt"
+#    } else {
+#        print > "bin/group3.txt"
+#    }
