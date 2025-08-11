7d14480c0289d74d2308f4f253bfc7bd88236a30
uqmmoise <1010930+sk33wiff@users.noreply.github.com>
2025-08-11 11:20:10 +1000

    - converting dlorList.spec.js [AD-318]

---
+++ playwright/lib/ckeditor.ts
@@ -0,0 +1,21 @@
+import { expect, Page } from '../test';
+
+export const assertCKEditorEmpty = async (page: Page, elementDataTestId: string) => {
+    await expect(
+        page.locator(`[data-testid="${elementDataTestId}"] .ck-editor__main p [data-cke-filler="true"]`),
+    ).toHaveCount(1);
+};
+
+export const readCKEditor = async (page: Page, testId: string) =>
+    (await page.locator(`[data-testid="${testId}"] .ck-editor__main p`).textContent()) ?? '';
+
+export const typeCKEditor = async (page: Page, containerTestId: string | undefined, content: string) => {
+    if (!containerTestId) {
+        await page.locator('[contenteditable]').fill(content);
+        return;
+    }
+    await page
+        .getByTestId(containerTestId)
+        .locator('[contenteditable]')
+        .fill(content);
+};
