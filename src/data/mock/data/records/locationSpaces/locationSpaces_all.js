157fcf4499dd378d33b0a0a9291162434ac8523c
Lea de Groot <l.degroot@library.uq.edu.au>
2025-08-20 08:47:50 +1000

    improve mock data [YT AD-362]

--- src/data/mock/data/records/locationSpaces/locationSpaces_all.js
+++ src/data/mock/data/records/locationSpaces/locationSpaces_all.js
@@ -1,8 +1,21 @@
 export default [{
     location_id: "123456",
     location_uuid: "987y_isjgt_9866",
-    location_title: "Forgen Smith Study room 1",
-    location_description: "a space in the Law library.",
+    location_title: "01-W437 Law Library",
+    location_precise: "Third floor [some position] Forgen Smith Building (Building 1) St Lucia Campus",
+    location_description: "a space in the Law library. lots of desscriptive words here",
+    location_photo: "https://campuses.uq.edu.au/files/35116/01-E107%20%28Resize%29.jpg", // https://campuses.uq.edu.au/list-rooms
     created_at: "2025-08-01 10:00:00",
     updated_at: "2025-08-01 11:00:00",
+    location_latitude: "-27.496955206561836",
+    location_longitude: "153.01308753792662",
+    facilities: [
+        {
+            facilityTypeId: 1,
+            facilityTypeDisplayName: 'Toilets nearby',
+        }, {
+            facilityTypeId: 2,
+            facilityTypeDisplayName: 'Toilets nearby',
+        }
+    ]
 }]
