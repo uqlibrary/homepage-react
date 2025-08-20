d04387cbaaa78177cfc57dd5e5ce63c48aa2b38d
Lea de Groot <l.degroot@library.uq.edu.au>
2025-08-21 09:22:18 +1000

    tweak interim layout [YT AD-362]

--- src/data/mock/data/records/locationSpaces/locationSpaces_all.js
+++ src/data/mock/data/records/locationSpaces/locationSpaces_all.js
@@ -1,10 +1,12 @@
 export default [{
     location_id: "123456",
     location_uuid: "987y_isjgt_9866",
-    location_title: "01-W437 Law Library",
-    location_precise: "Third floor [some position] Forgen Smith Building (Building 1) St Lucia Campus",
-    location_description: "a space in the Law library. lots of desscriptive words here",
+    location_title: "01-W431 Law Library",
+    location_precise: "Third floor [some position] Forgen Smith Building St Lucia Campus",
+    location_description: "A space in the Law library. lots of descriptive words here",
     location_photo: "https://campuses.uq.edu.au/files/35116/01-E107%20%28Resize%29.jpg", // https://campuses.uq.edu.au/list-rooms
+    location_photo_description: 'a large room with 6 large round tables, each wih multiple chairs',
+    location_type: 'Collaborative space',
     created_at: "2025-08-01 10:00:00",
     updated_at: "2025-08-01 11:00:00",
     location_latitude: "-27.496955206561836",
@@ -15,7 +17,74 @@ export default [{
             facilityTypeDisplayName: 'Toilets nearby',
         }, {
             facilityTypeId: 2,
-            facilityTypeDisplayName: 'Toilets nearby',
+            facilityTypeDisplayName: 'Group study tables',
         }
     ]
+}, {
+    location_id: "1234544",
+    location_uuid: "9867y_isjgt_9866",
+    location_title: "N107A Central Library",
+    location_precise: "Central Library, St Lucia Campus",
+    location_description: "A space in Central. lots of descriptive words here",
+    location_photo: "",
+    location_photo_description: "",
+    location_type: 'Computer room',
+    created_at: "2025-08-01 10:00:00",
+    updated_at: "2025-08-01 11:00:00",
+    location_latitude: "-27.496955206561836",
+    location_longitude: "153.01308753792662",
+    facilities: [
+        {
+            facilityTypeId: 5,
+            facilityTypeDisplayName: 'Cafe nearby',
+        }, {
+            facilityTypeId: 6,
+            facilityTypeDisplayName: 'Standing Desks',
+        }
+    ]
+}, {
+    location_id: "43534",
+    location_uuid: "987y_isjgt_9867",
+    location_title: "46-342/343",
+    location_precise: "Third floor [some position] Andrew N. Liveris Building St Lucia Campus",
+    location_description: "A space in the Liveris building. lots of descriptive words here",
+    location_photo: "https://campuses.uq.edu.au/files/35424/46-342-343.JPG", // https://campuses.uq.edu.au/list-rooms
+    location_photo_description: 'a large room with many tables, each wih 4 chairs',
+    created_at: "2025-08-01 10:00:00",
+    updated_at: "2025-08-01 11:00:00",
+    location_latitude: "-27.50046410327122",
+    location_longitude: "153.0141740262594",
+    facilities: [
+        {
+            facilityTypeId: 3,
+            facilityTypeDisplayName: 'Natural light',
+        }, {
+            facilityTypeId: 4,
+            facilityTypeDisplayName: 'Moderated',
+        }, {
+            facilityTypeId: 2,
+            facilityTypeDisplayName: 'Group study tables',
+        }
+    ]
+// }, {
+//     location_id: "123456",
+//     location_uuid: "987y_isjgt_9866",
+//     location_title: "01-W431 Law Library",
+//     location_precise: "Third floor [some position] Forgen Smith Building St Lucia Campus",
+//     location_description: "A space in the Law library. lots of descriptive words here",
+//     location_photo: "https://campuses.uq.edu.au/files/35116/01-E107%20%28Resize%29.jpg", // https://campuses.uq.edu.au/list-rooms
+//     location_photo_description: 'a large room with 6 large round tables, each wih multiple chairs',
+//     created_at: "2025-08-01 10:00:00",
+//     updated_at: "2025-08-01 11:00:00",
+//     location_latitude: "-27.496955206561836",
+//     location_longitude: "153.01308753792662",
+//     facilities: [
+//         {
+//             facilityTypeId: 1,
+//             facilityTypeDisplayName: 'Toilets nearby',
+//         }, {
+//             facilityTypeId: 2,
+//             facilityTypeDisplayName: 'Group study tables',
+//         }
+//     ]
 }]
--- src/modules/Pages/SpacesLocations/SpacesLocationList.js
+++ src/modules/Pages/SpacesLocations/SpacesLocationList.js
@@ -2,10 +2,23 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import { Grid } from '@mui/material';
+import { styled } from '@mui/material/styles';
 
 import { StandardPage } from 'modules/SharedComponents/Toolbox/StandardPage';
 import { StandardCard } from 'modules/SharedComponents/Toolbox/StandardCard';
 import { InlineLoader } from '../../SharedComponents/Toolbox/Loaders';
+
+const StyledStandardCard = styled(StandardCard)(() => ({
+    '& .MuiCardHeader-root': {
+        paddingBottom: 0,
+    },
+    '& .MuiCardContent-root': {
+        paddingBlock: 0,
+    },
+}));
+const StyledLocationPhoto = styled('img')(() => ({
+    maxWidth: '100%',
+}));
 
 export const SpacesLocationList = ({
     actions,
@@ -22,48 +35,65 @@ export const SpacesLocationList = ({
 
     return (
         <StandardPage title="Library bookable locations">
-            <StandardCard standardCardId="location-list-card">
-                <Grid container spacing={3}>
-                    {(() => {
-                        if (!!locationSpaceListLoading) {
-                            return (
-                                <Grid item xs={12} md={9} sx={{ marginTop: '12px' }}>
-                                    <StandardCard fullHeight>
-                                        <InlineLoader message="Loading" />
-                                    </StandardCard>
-                                </Grid>
-                            );
-                        } else if (!!locationSpaceListError) {
-                            return (
-                                <Grid item xs={12} md={9} sx={{ marginTop: '12px' }}>
-                                    <StandardCard fullHeight>
-                                        <p>Something went wrong - please try again later.</p>
-                                    </StandardCard>
-                                </Grid>
-                            );
-                        } else if (!locationSpaceList || locationSpaceList.length === 0) {
-                            return (
-                                <Grid item xs={12} md={9} sx={{ marginTop: '12px' }}>
-                                    <StandardCard fullHeight>
-                                        <p>No locations found.</p>
-                                    </StandardCard>
-                                </Grid>
-                            );
-                        } else {
-                            return locationSpaceList.map(object => {
+            <section aria-live="assertive">
+                <StandardCard standardCardId="location-list-card" noPadding noHeader style={{ border: 'none' }}>
+                    <Grid container spacing={3}>
+                        {(() => {
+                            if (!!locationSpaceListLoading) {
                                 return (
                                     <Grid item xs={12} md={9} sx={{ marginTop: '12px' }}>
-                                        <StandardCard fullHeight>
-                                            <h2>{object.location_title}</h2>
-                                            <p>{object.location_description}</p>
-                                        </StandardCard>
+                                        <StyledStandardCard fullHeight>
+                                            <InlineLoader message="Loading" />
+                                        </StyledStandardCard>
                                     </Grid>
                                 );
-                            });
-                        }
-                    })()}
-                </Grid>
-            </StandardCard>
+                            } else if (!!locationSpaceListError) {
+                                return (
+                                    <Grid item xs={12} md={9} sx={{ marginTop: '12px' }}>
+                                        <StyledStandardCard fullHeight>
+                                            <p>Something went wrong - please try again later.</p>
+                                        </StyledStandardCard>
+                                    </Grid>
+                                );
+                            } else if (!locationSpaceList || locationSpaceList.length === 0) {
+                                return (
+                                    <Grid item xs={12} md={9} sx={{ marginTop: '12px' }}>
+                                        <StyledStandardCard fullHeight>
+                                            <p>No locations found.</p>
+                                        </StyledStandardCard>
+                                    </Grid>
+                                );
+                            } else {
+                                return locationSpaceList.map(object => {
+                                    return (
+                                        <Grid item xs={12} md={9} sx={{ marginTop: '12px' }}>
+                                            <StyledStandardCard fullHeight title={object?.location_title}>
+                                                <p>{object?.location_description}</p>
+                                                {object?.location_photo && (
+                                                    <StyledLocationPhoto
+                                                        src={object?.location_photo}
+                                                        alt={object?.location_photo_description}
+                                                    />
+                                                )}
+                                                {object?.facilities?.length > 0 && (
+                                                    <>
+                                                        <h3>Facilities</h3>
+                                                        <ul>
+                                                            {object?.facilities?.map(facility => {
+                                                                return <li>{facility.facilityTypeDisplayName}</li>;
+                                                            })}
+                                                        </ul>
+                                                    </>
+                                                )}
+                                            </StyledStandardCard>
+                                        </Grid>
+                                    );
+                                });
+                            }
+                        })()}
+                    </Grid>
+                </StandardCard>
+            </section>
         </StandardPage>
     );
 };
