b11a793cabb8af3b846a152b1fc801719e2f7fa4
uqmmoise <1010930+sk33wiff@users.noreply.github.com>
2025-08-25 16:28:42 +1000

    - fixing flaky test [AD-318]

--- playwright/tests/homepage/libraryUpdates.spec.ts
+++ playwright/tests/homepage/libraryUpdates.spec.ts
@@ -1,4 +1,4 @@
-import { test, expect } from '@uq/pw/test';
+import { test, expect, Locator } from '@uq/pw/test';
 import { assertAccessibility } from '@uq/pw/lib/axe';
 
 interface BoundingBox {
@@ -12,6 +12,12 @@ test.describe('LibraryUpdates', () => {
     test.describe('content', () => {
         // CY to PW conversion note: added some margin to avoid parallel testing rand. failures
         const BOUNDING_BOX_TOLERANCE = 2;
+        const getBoundingBox = async (el: Locator): Promise<BoundingBox> => {
+            await expect.poll(async () => (await el.boundingBox()) !== null, { timeout: 5000 }).toBeTruthy();
+            const box = await el.boundingBox();
+            if (!box) throw new Error(`Unable to get bounding box: ${el}`);
+            return box;
+        };
 
         test('loads as expected on desktop', async ({ page }) => {
             await page.goto('/');
@@ -29,22 +35,22 @@ test.describe('LibraryUpdates', () => {
             //  \----------------/
             //  XXXX   XXXX   XXXX
             await page.evaluate(() => window.scrollBy(0, document.body.scrollHeight));
-            const firstBox = (await page.getByTestId('drupal-article-0').boundingBox()) as BoundingBox;
+            const firstBox = await getBoundingBox(page.getByTestId('drupal-article-0'));
             // CY to PW conversion note: value below from 77 to 84 to avoid parallel testing rand. failures
             expect(firstBox.x).toBeLessThan(84 + BOUNDING_BOX_TOLERANCE);
 
-            const secondBox = (await page.getByTestId('drupal-article-1').boundingBox()) as BoundingBox;
+            const secondBox = await getBoundingBox(page.getByTestId('drupal-article-1'));
             expect(Math.abs(secondBox.x - firstBox.x)).toBeLessThan(BOUNDING_BOX_TOLERANCE);
             expect(secondBox.y).toBeGreaterThan(firstBox.y + firstBox.height - BOUNDING_BOX_TOLERANCE);
 
-            const thirdBox = (await page.getByTestId('drupal-article-2').boundingBox()) as BoundingBox;
+            const thirdBox = await getBoundingBox(page.getByTestId('drupal-article-2'));
             expect(Math.abs(thirdBox.y - secondBox.y)).toBeLessThan(BOUNDING_BOX_TOLERANCE);
             expect(Math.abs(thirdBox.y + thirdBox.height - (secondBox.y + secondBox.height))).toBeLessThan(
                 BOUNDING_BOX_TOLERANCE,
             );
             expect(thirdBox.x).toBeGreaterThan(secondBox.x + secondBox.width - BOUNDING_BOX_TOLERANCE);
 
-            const fourthBox = (await page.getByTestId('drupal-article-3').boundingBox()) as BoundingBox;
+            const fourthBox = await getBoundingBox(page.getByTestId('drupal-article-3'));
             expect(Math.abs(fourthBox.y - secondBox.y)).toBeLessThan(BOUNDING_BOX_TOLERANCE);
             expect(Math.abs(fourthBox.y + fourthBox.height - (secondBox.y + secondBox.height))).toBeLessThan(
                 BOUNDING_BOX_TOLERANCE,
@@ -68,17 +74,14 @@ test.describe('LibraryUpdates', () => {
             //  \-----------/
             //  XXXXX   XXXXX
             //  XXXXX
-            const first = page.getByTestId('drupal-article-0');
-            const firstBox = (await first.boundingBox()) as BoundingBox;
+            const firstBox = await getBoundingBox(page.getByTestId('drupal-article-0'));
             expect(firstBox.x).toBeLessThan(30 + BOUNDING_BOX_TOLERANCE);
 
-            const second = page.getByTestId('drupal-article-1');
-            const secondBox = (await second.boundingBox()) as BoundingBox;
+            const secondBox = await getBoundingBox(page.getByTestId('drupal-article-1'));
             expect(Math.abs(secondBox.x - firstBox.x)).toBeLessThan(BOUNDING_BOX_TOLERANCE);
-            expect(secondBox.y).toBeGreaterThan(firstBox.y + firstBox.height - BOUNDING_BOX_TOLERANCE - 10);
+            expect(secondBox.y).toBeGreaterThan(firstBox.y + firstBox.height - BOUNDING_BOX_TOLERANCE - 12);
 
-            const third = page.getByTestId('drupal-article-2');
-            const thirdBox = (await third.boundingBox()) as BoundingBox;
+            const thirdBox = await getBoundingBox(page.getByTestId('drupal-article-2'));
             expect(Math.abs(thirdBox.y - secondBox.y)).toBeLessThan(BOUNDING_BOX_TOLERANCE);
             expect(Math.abs(thirdBox.y + thirdBox.height - (secondBox.y + secondBox.height))).toBeLessThan(
                 BOUNDING_BOX_TOLERANCE,
@@ -105,28 +108,24 @@ test.describe('LibraryUpdates', () => {
             //    XXXX
             //    XXXX
             //    XXXX
-            const first = page.getByTestId('drupal-article-0');
-            const firstBox = (await first.boundingBox()) as BoundingBox;
+            const firstBox = await getBoundingBox(page.getByTestId('drupal-article-0'));
             expect(firstBox.x).toBeLessThan(26 + BOUNDING_BOX_TOLERANCE);
 
-            const second = page.getByTestId('drupal-article-1');
-            const secondBox = (await second.boundingBox()) as BoundingBox;
+            const secondBox = await getBoundingBox(page.getByTestId('drupal-article-1'));
             expect(Math.abs(secondBox.x - firstBox.x)).toBeLessThan(BOUNDING_BOX_TOLERANCE);
             expect(Math.abs(secondBox.x + secondBox.width - (firstBox.x + firstBox.width))).toBeLessThan(
                 BOUNDING_BOX_TOLERANCE,
             );
             expect(secondBox.y).toBeGreaterThan(firstBox.y + firstBox.height - BOUNDING_BOX_TOLERANCE);
 
-            const third = page.getByTestId('drupal-article-2');
-            const thirdBox = (await third.boundingBox()) as BoundingBox;
+            const thirdBox = await getBoundingBox(page.getByTestId('drupal-article-2'));
             expect(Math.abs(thirdBox.x - secondBox.x)).toBeLessThan(BOUNDING_BOX_TOLERANCE);
             expect(Math.abs(thirdBox.x + thirdBox.width - (secondBox.x + secondBox.width))).toBeLessThan(
                 BOUNDING_BOX_TOLERANCE,
             );
             expect(thirdBox.y).toBeGreaterThan(secondBox.y + secondBox.height - BOUNDING_BOX_TOLERANCE);
 
-            const fourth = page.getByTestId('drupal-article-3');
-            const fourthBox = (await fourth.boundingBox()) as BoundingBox;
+            const fourthBox = await getBoundingBox(page.getByTestId('drupal-article-3'));
             expect(Math.abs(fourthBox.x - secondBox.x)).toBeLessThan(BOUNDING_BOX_TOLERANCE);
             expect(Math.abs(fourthBox.x + fourthBox.width - (secondBox.x + secondBox.width))).toBeLessThan(
                 BOUNDING_BOX_TOLERANCE,
