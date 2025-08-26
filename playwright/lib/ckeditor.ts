6684786561c878b8d55bb9b3f025a98e7e95b548
uqmmoise <1010930+sk33wiff@users.noreply.github.com>
2025-08-26 15:59:56 +1000

    Revert "- converting dlorEdit.spec.js [AD-318]"
    This reverts commit b0e5bc19

--- playwright/lib/ckeditor.ts
+++ playwright/lib/ckeditor.ts
@@ -1,11 +1,13 @@
-import { Page } from '../test';
+import { expect, Page } from '../test';
 
-export const readCKEditor = async (page: Page, containerTestId?: string) => {
-    if (!containerTestId) {
-        return (await page.locator('.ck-content').textContent()) ?? '';
-    }
-    return (await page.locator(`[data-testid="${containerTestId}"] .ck-editor__main p`).textContent()) ?? '';
+export const assertCKEditorEmpty = async (page: Page, elementDataTestId: string) => {
+    await expect(
+        page.locator(`[data-testid="${elementDataTestId}"] .ck-editor__main p [data-cke-filler="true"]`),
+    ).toHaveCount(1);
 };
+
+export const readCKEditor = async (page: Page, testId: string) =>
+    (await page.locator(`[data-testid="${testId}"] .ck-editor__main p`).textContent()) ?? '';
 
 export const typeCKEditor = async (page: Page, containerTestId: string | undefined, content: string) => {
     if (!containerTestId) {
