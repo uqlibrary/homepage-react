d326388c9942fb878a0171f14b28688f74817df8
uqmmoise <1010930+sk33wiff@users.noreply.github.com>
2025-08-26 15:57:04 +1000

    Revert "- fixing flaky test"
    This reverts commit cc208dbffdbfac8c9bdd060fc0b41e482cd49e20.

--- playwright/tests/adminPages/testandtag/manage/inspectionDevices.spec.ts
+++ playwright/tests/adminPages/testandtag/manage/inspectionDevices.spec.ts
@@ -19,9 +19,7 @@ test.describe('Test and Tag manage inspection devices', () => {
         await checkBaseline(page);
         const dueDateCell = await getFieldValue(page, 'device_calibration_due_date', 2);
         await expect(dueDateCell.locator('svg')).toBeVisible();
-        await assertAccessibility(page, '[data-testid="StandardPage"]', {
-            disabledRules: ['aria-required-children', 'aria-progressbar-name'],
-        });
+        await assertAccessibility(page, '[data-testid="StandardPage"]', { disabledRules: ['aria-required-children'] });
     });
 
     test('has breadcrumbs', async ({ page }) => {
