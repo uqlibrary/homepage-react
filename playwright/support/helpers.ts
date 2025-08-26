a1711df8570a23046465b7342c1b7b81f8bfee13
uqmmoise <1010930+sk33wiff@users.noreply.github.com>
2025-08-26 16:01:21 +1000

    Revert "- converting alertList.spec.js [AD-318]"
    This reverts commit 37ff2d6c

--- playwright/support/helpers.ts
+++ playwright/support/helpers.ts
@@ -8,6 +8,17 @@ export async function clickSVGButton(page: Page, selector: string) {
     await page.locator(selector).click();
 }
 
+export function readingListLength(courseReadingList: any): number {
+    return (
+        (!!courseReadingList.reading_lists &&
+            courseReadingList.reading_lists.length > 0 &&
+            !!courseReadingList.reading_lists[0] &&
+            !!courseReadingList.reading_lists[0].totalCount &&
+            courseReadingList.reading_lists[0].totalCount) ||
+        0
+    );
+}
+
 export async function dateHasValue(page: Page, dateField: string, expectedDate: string) {
     const dateValue = await page.locator(dateField).getAttribute('value');
     expect(dateValue).toMatch(new RegExp(`^${expectedDate}`));
