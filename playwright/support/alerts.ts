5caa7fb821faf80cc536d26f14c57faf87f5ae27
uqmmoise <1010930+sk33wiff@users.noreply.github.com>
2025-08-26 16:01:22 +1000

    Revert "- converting alertView.spec.js"
    This reverts commit 3b6be68ef94b0a4adeedc3937110066e9737f81a.

--- playwright/support/alerts.ts
+++
@@ -1,9 +0,0 @@
-import { expect, Page } from '../test';
-
-export async function hasAWorkingHelpButton(page: Page) {
-    await expect(page.getByTestId('admin-alerts-help-example')).not.toBeVisible();
-    await page.getByTestId('admin-alerts-help-button').click();
-    await expect(page.getByTestId('admin-alerts-help-example')).toBeVisible();
-    await page.getByRole('button', { name: 'Close' }).click();
-    await expect(page.getByTestId('admin-alerts-help-example')).not.toBeVisible();
-}
