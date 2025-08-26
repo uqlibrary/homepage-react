77c593304ae78a8e11f12ae8873f5f5531905d5d
uqmmoise <1010930+sk33wiff@users.noreply.github.com>
2025-08-26 15:56:58 +1000

    Revert "- fixing flaky test"
    This reverts commit ef1bf886f2a99a8ad615f8c5b1a56b32322ec048.

--- playwright/lib/axe.ts
+++ playwright/lib/axe.ts
@@ -6,20 +6,20 @@ export const defaultDisabledRules = ['aria-required-children', 'aria-progressbar
 
 export const assertAccessibility = async (
     page: Page,
-    selector: string,
+    selector?: string,
     options?: {
         rules?: string[];
         disabledRules?: string[];
         includedImpacts?: string[];
     },
 ) => {
-    await expect(async () => await expect(page.locator(selector)).toBeVisible({ timeout: 500 })).toPass();
-
     const builder = new AxeBuilder({ page }).withTags(['wcag2a', 'wcag2aa', 'wcag21a', 'wcag21aa', 'best-practice']);
     const includedImpacts = options?.includedImpacts || defaultIncludedImpacts;
     const disableRules = options?.disabledRules || defaultDisabledRules;
 
-    builder.include(selector);
+    if (selector) {
+        builder.include(selector);
+    }
     if (options?.rules?.length) {
         builder.withRules(options.rules);
     }
