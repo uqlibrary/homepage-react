d3b879224312cb3a307c0e6d6abbf2a4fecc60d1
uqmmoise <1010930+sk33wiff@users.noreply.github.com>
2025-08-26 16:00:18 +1000

    Revert "- converting assetReportByFilters.spec.js"
    This reverts commit 8adcbfa131e5a858da217bc83ddb3f33dda30047.

--- playwright/tests/adminPages/testandtag/reports/helpers.ts
+++
@@ -1,9 +0,0 @@
-import { Page } from '@uq/pw/test';
-
-export const getFieldValue = async (page: Page, dataField: string, rowIndex: number) =>
-    page.locator(`div[data-rowindex='${rowIndex}'] > div[data-field='${dataField}']`);
-
-export const forcePageRefresh = async (page: Page) => {
-    await page.getByTestId('test_tag_header-navigation-dashboard').click();
-    await page.goBack();
-};
