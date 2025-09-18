27a5d15fc2541406e2a11efc380a985afda6a57a
Steve Lancaster <90990468+SteveLancaster@users.noreply.github.com>
2025-09-18 16:40:08 +1000

    Update codebuild-test.sh

--- bin/codebuild-test.sh
+++ bin/codebuild-test.sh
@@ -148,9 +148,9 @@ case "$PIPE_NUM" in
 esac
 
 # Copy empty file to prevent a build failure as we only report on combined cobertura coverage when $TEST_COVERAGE=1
-if [[ $CODE_COVERAGE_REQUIRED == 1 ]]; then
+#if [[ $CODE_COVERAGE_REQUIRED == 1 ]]; then
     mkdir -p coverage && cp cobertura-sample-coverage.xml coverage/cobertura-coverage.xml
-fi
+#fi
 
 printf "DEBUG: CODE_COVERAGE_REQUIRED is: %s\n" "$CODE_COVERAGE_REQUIRED"
 printf "DEBUG: CONTENTS OF THE COVERAGE DIRECTORY:\n"
