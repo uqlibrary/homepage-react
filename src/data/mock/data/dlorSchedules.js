acdaf3dec30ac194c723856651c730b9d0585f37
Steve Lancaster <90990468+SteveLancaster@users.noreply.github.com>
2025-12-13 14:32:23 +1000

    add finished schedule to mock

--- src/data/mock/data/dlorSchedules.js
+++ src/data/mock/data/dlorSchedules.js
@@ -41,5 +41,21 @@ export const dlorSchedules = {
                 "98s0_dy5k3_98h4"
             ]
         },
+        {
+            "schedule_id": 99,
+            "schedule_name": "completed",
+            "schedule_start_date": "2025-12-22",
+            "schedule_end_date": "2025-12-23",
+            "created_at": null,
+            "updated_at": null,
+            "schedule_status": true,
+            "object_count": 2,
+            "schedule_running": false,
+            "schedule_processed": true,
+            "schedule_objects": [
+                "987y_isjgt_9866",
+                "98s0_dy5k3_98h4"
+            ]
+        },
     ]
 }
