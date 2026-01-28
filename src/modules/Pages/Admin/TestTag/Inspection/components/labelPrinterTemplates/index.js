eba5968ebdf7351ddea29accd22fd62223e7e3fa
Lee S <lsibbald100+git@gmail.com>
2026-01-28 11:25:44 +1000

    [AD-507] Added specific serial number template reference, for cases on Windows where only the serial number is returned in the list of available printers. Adjusted the normaliser for printer name to add a $ to any printer name that starts with a number (i.e. serial numbers)

--- src/modules/Pages/Admin/TestTag/Inspection/components/labelPrinterTemplates/index.js
+++ src/modules/Pages/Admin/TestTag/Inspection/components/labelPrinterTemplates/index.js
@@ -1,3 +1,5 @@
 export { default as emulator } from './emulator';
 export { default as gk420t } from './gk420t';
+export { default as $29J120602579 } from './gk420t';
 export { default as gk888t } from './gk888t';
+export { default as $19j153101586 } from './gk888t';
