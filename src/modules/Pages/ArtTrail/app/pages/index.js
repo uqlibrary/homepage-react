020218f1d35f469a324d8997e530fcd7922aaae6
Lee S <lsibbald100+git@gmail.com>
2026-08-11 15:41:02 +1000

    [AD-1189] Saltwater croc page

--- src/modules/Pages/ArtTrail/app/pages/index.js
+++ src/modules/Pages/ArtTrail/app/pages/index.js
@@ -4,5 +4,6 @@ import SandHills from './SandHills';
 import DevilMountainLizard from './DevilMountainLizard';
 import Tingari from './Tingari';
 import Kunawarritji from './Kunawarritji';
+import Pikkuw from './Pikkuw';
 
-export const trailPages = [Welcome, PenuTjukurpa, SandHills, DevilMountainLizard, Tingari, Kunawarritji];
+export const trailPages = [Welcome, PenuTjukurpa, SandHills, DevilMountainLizard, Tingari, Kunawarritji, Pikkuw];
