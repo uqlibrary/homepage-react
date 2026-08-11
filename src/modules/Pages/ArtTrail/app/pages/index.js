7172b04b2d78ea6415ac7142fa6fd3a1225b15bc
Lee S <lsibbald100+git@gmail.com>
2026-08-11 17:14:28 +1000

    [AD-1189] Added Whispers page

--- src/modules/Pages/ArtTrail/app/pages/index.js
+++ src/modules/Pages/ArtTrail/app/pages/index.js
@@ -5,5 +5,15 @@ import DevilMountainLizard from './DevilMountainLizard';
 import Tingari from './Tingari';
 import Kunawarritji from './Kunawarritji';
 import Pikkuw from './Pikkuw';
+import Whispers from './Whispers';
 
-export const trailPages = [Welcome, PenuTjukurpa, SandHills, DevilMountainLizard, Tingari, Kunawarritji, Pikkuw];
+export const trailPages = [
+    Welcome,
+    PenuTjukurpa,
+    SandHills,
+    DevilMountainLizard,
+    Tingari,
+    Kunawarritji,
+    Pikkuw,
+    Whispers,
+];
