62dc1770d0952ed68c780073a0a84dbcf959a036
uqmmoise <1010930+sk33wiff@users.noreply.github.com>
2026-09-22 10:25:25 +1000

    - Creating DigitalLearningObjects/Form/File/Dialogs [AD-1200]

---
+++ src/modules/Pages/Admin/DigitalLearningObjects/Form/File/Dialogs.js
@@ -0,0 +1,75 @@
+import React from 'react';
+import { ConfirmationBox } from '../../../../../SharedComponents/Toolbox/ConfirmDialogBox';
+
+const Dialog = ({ title, text }) => (
+    <ConfirmationBox
+        actionButtonColor="primary"
+        actionButtonVariant="contained"
+        confirmationBoxId="dlor-object-file-dialog"
+        hideActionButton
+        hideCancelButton
+        isOpen
+        locale={{ confirmationTitle: title, confirmationMessage: text }}
+    />
+);
+
+const Dialogs = ({
+    existingFile,
+    fileToBeUploaded,
+    uploadProgress = 0,
+    deleting,
+    deleteError,
+    uploading,
+    uploadError,
+}) => (
+    <>
+        {deleting && (
+            <Dialog
+                open
+                title="Processing..."
+                text={
+                    <>
+                        Deleting removed file <b>{existingFile.name}</b>, please wait.
+                    </>
+                }
+            />
+        )}
+        {deleteError && (
+            <Dialog
+                open
+                title="Error"
+                text={
+                    <>
+                        Error while deleting file <b>{existingFile.name}</b>.<br />
+                        Please refresh the page and try again.
+                    </>
+                }
+            />
+        )}
+        {uploading && (
+            <Dialog
+                open
+                title={`Processing... ${uploadProgress}%`}
+                text={
+                    <>
+                        Uploading file <b>{fileToBeUploaded.name}</b>, please wait.
+                    </>
+                }
+            />
+        )}
+        {uploadError && (
+            <Dialog
+                open
+                title="Error"
+                text={
+                    <>
+                        Error while uploading file <b>{fileToBeUploaded.name}</b>.<br />
+                        Please refresh the page and try again.
+                    </>
+                }
+            />
+        )}
+    </>
+);
+
+export default React.memo(Dialogs);
